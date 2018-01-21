2e8d442bd17868e7cdaa539837efe2be1652e189
George333123 <georgegillams@hotmail.co.uk>
2018-01-21 10:06:43 +0000

    Add photoshop page

--- src/Pages/Articles/SwitchToVim.jsx
+++ src/Pages/Articles/SwitchToVim.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import BpkText from 'bpk-component-text';
 import Section from './../../components/Section';
 import SubSection from './../../components/SubSection';
 import ArticleDate from '../../components/ArticleDate';
 import Comments from '../../components/Comments';
+import TextLink from '../../components/TextLink';
 
 // import STYLES from '../pages.scss';
 //
@@ -29,11 +29,17 @@ const SwitchToVim = (props) => {
         I started by getting hooked on vim-adventures - an online game designed to teach you all the Vim commands. It was odd, because I haven&apos;t played a video game for at least 5 years, but something about it was strangely addictive (maybe the fact that it wasn&apos;t group coursework?!).
 	<br />
           <br />
-          The next step was to install vim-mode-plus for Atom (currently my JS editor of choice). Tbh I thought my Atom setup wasn&apos;t half bad - I was barely touching the mouse and was often editing more than 1 line of code at a time - but adding in vim features made me realise that I was actually missing out on a lot of handy shortcuts. Before long I'd ditched Atom for Neovim, enabled Vim mode in my .zshrc and installed Vimium for Firefox as I wanted to use Vim everywhere.
+          The next step was to install <TextLink href="https://atom.io/packages/vim-mode-plus">vim-mode-plus</TextLink> for Atom (currently my JS editor of choice). Tbh I thought my Atom setup wasn&apos;t half bad - I was barely touching the mouse and was often editing more than 1 line of code at a time - but adding in vim features made me realise that I was actually missing out on a lot of handy shortcuts. Before long I'd ditched Atom for Neovim, enabled Vim mode in my .zshrc and installed Vimium for Firefox as I wanted to use Vim everywhere.
         <br />
           <br />
           <SubSection name="Learning Curve">
           A lot of people told me that learning Vim was a terrible mistake simply due to the learning curve. A lot of bloggers deemed their productivity in the coming weeks far too precious to invest time in increasing their productivity long-term, but despite my work-load and approaching deadlines I stuck with it. I guess I spent around a week at half speed as I found my way around and configured all the Plugins I wanted, and maybe I haven't yet even got back up to full-speed, but I'm getting faster every day. I've learnt a new, awesome tool which has changed my outlook on how code should be written. And so far I've only scratched the surface of what it is capable of. So, was it worth it? Hell yes!
+        <br />
+            <br />
+            <SubSection name="My new workflow">
+            As it happens, I've switched back to Atom for it's modern UI, powerful search features and the packages I'm already familiar with. But I'm certainly going to be keeping <TextLink href="https://atom.io/packages/vim-mode-plus">vim-mode-plus</TextLink> and using the mouse a lot less!
+            My speed of work is much greater for learning and using Vim, and the amount of time I will save in the coming weeks/months/years will definitely be worth the learning process.
+            </SubSection>
           </SubSection>
         </SubSection>
       </Section>
