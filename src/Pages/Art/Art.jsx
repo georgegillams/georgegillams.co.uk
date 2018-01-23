c5a024af2c09133acd54e03cd54e380903cfa271
George333123 <georgegillams@hotmail.co.uk>
2018-01-23 13:30:52 +0000

    Prettier

--- src/Pages/Art/Art.jsx
+++ src/Pages/Art/Art.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import BpkImage, {
-  withLazyLoading,
-  withLoadingBehavior
-} from "bpk-component-image";
+import BpkImage, { withLazyLoading, withLoadingBehavior } from "bpk-component-image";
 import Section from "../../components/Section";
 import SubSection from "../../components/SubSection";
 import Comments from "../../components/Comments";
@@ -18,11 +15,8 @@ import STYLES from "../pages.scss";
 const getClassName = className => STYLES[className] || "UNKNOWN";
 const PAGE_ID = 857216;
 const documentIfExists = typeof window !== "undefined" ? document : null;
-const FadingLazyLoadedImage = withLoadingBehavior(
-  withLazyLoading(BpkImage, documentIfExists)
-);
+const FadingLazyLoadedImage = withLoadingBehavior(withLazyLoading(BpkImage, documentIfExists));
 
- 
 const Art = props => {
   const { className, ...rest } = props;
   const classNameFinal = [getClassName("pages__page")];
@@ -34,9 +28,8 @@ const Art = props => {
     <main className={classNameFinal.join(" ")} {...rest}>
       <Section name="Food is art! (...sometimes)">
         <SubSection name="Guinness cake">
-          I like spending time on food presentation when the opportunity calls
-          for it. So I created this masterpiece to share with the office and
-          celebrate a legendary drink!
+          I like spending time on food presentation when the opportunity calls for it. So I created
+          this masterpiece to share with the office and celebrate a legendary drink!
           <br />
           <br />
           <FadingLazyLoadedImage
@@ -48,9 +41,9 @@ const Art = props => {
           />
           <br />
           <SubSection name="Ratatouille">
-            To get us in the mood for Disneyland, I cooked up this Ratatouille
-            in the style of the dish served in the film. It came out better than
-            I expected... Pretty pleased with the result!
+            To get us in the mood for Disneyland, I cooked up this Ratatouille in the style of the
+            dish served in the film. It came out better than I expected... Pretty pleased with the
+            result!
             <br />
             <br />
             <FadingLazyLoadedImage
@@ -65,11 +58,10 @@ const Art = props => {
         </SubSection>
         <Section name="Photoshop">
           <SubSection name="Tulips exploding with light">
-            For some reason the idea of light exploding out of tulips popped
-            into my mind, so I went out to find some and made it a reality. I
-            used a similar effect in Art that I had used in the past to create
-            beams of sunlight breaking through the clouds, and then darkened the
-            background a little.
+            For some reason the idea of light exploding out of tulips popped into my mind, so I went
+            out to find some and made it a reality. I used a similar effect in Art that I had used
+            in the past to create beams of sunlight breaking through the clouds, and then darkened
+            the background a little.
             <br />
             <br />
             <FadingLazyLoadedImage
@@ -82,8 +74,8 @@ const Art = props => {
             <br />
           </SubSection>
           <SubSection name="Miss Saigon sketch">
-            With Miss Saigon coming to cinemas soon for one day only, I was
-            inspired to draw the production logo (aka tempted to procrastinate).
+            With Miss Saigon coming to cinemas soon for one day only, I was inspired to draw the
+            production logo (aka tempted to procrastinate).
             <br />
             <br />
             <FadingLazyLoadedImage
@@ -95,9 +87,9 @@ const Art = props => {
             />
             <br />
             <SubSection name="Dual-carriageway light-painting (in post)">
-              One evening after leaving work at an unearthly hour, I shot this
-              uninspired photo. In Photoshop, I then used the brush tool and
-              some layer styles to create a 'painting with light' effect.
+              One evening after leaving work at an unearthly hour, I shot this uninspired photo. In
+              Photoshop, I then used the brush tool and some layer styles to create a 'painting with
+              light' effect.
               <br />
               <br />
               <FadingLazyLoadedImage
@@ -125,4 +117,4 @@ Art.defaultProps = {
   className: null
 };
 
- export default Art;
+export default Art;
--- src/Pages/Articles/UkBankSecurity.jsx
+++ src/Pages/Articles/UkBankSecurity.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import BpkImage, {
-  withLazyLoading,
-  withLoadingBehavior
-} from "bpk-component-image";
+import BpkImage, { withLazyLoading, withLoadingBehavior } from "bpk-component-image";
 import TextLink from "../../components/TextLink";
 import Section from "../../components/Section";
 import SubSection from "../../components/SubSection";
@@ -16,13 +13,10 @@ import STYLES from "../pages.scss";
 
 const getClassName = className => STYLES[className] || "UNKNOWN";
 const documentIfExists = typeof window !== "undefined" ? document : null;
-const FadingLazyLoadedImage = withLoadingBehavior(
-  withLazyLoading(BpkImage, documentIfExists)
-);
+const FadingLazyLoadedImage = withLoadingBehavior(withLazyLoading(BpkImage, documentIfExists));
 const DATE_WRITTEN = new Date(2018, 1, 20, 14, 52, 0);
 const PAGE_ID = 827461;
 
- 
 const UkBankSecurity = props => {
   const { className, ...rest } = props;
   const classNameFinal = [];
@@ -33,24 +27,20 @@ const UkBankSecurity = props => {
   return (
     <main className={classNameFinal.join(" ")} {...rest}>
       <Section name="UK Bank Security">
-        When logging into a number of UK bank websites, they ask for particular
-        characters from a password or memorable word. Some banks implement this
-        as an additional layer of security after checking your full username and
-        password (TSB, Halifax, John Lewis), which is effective in preventing
-        keylogger attacks from gaining access (at least for several days). Other
-        banks (Santander, RBS) implement this to the detriment of their
-        security.
+        When logging into a number of UK bank websites, they ask for particular characters from a
+        password or memorable word. Some banks implement this as an additional layer of security
+        after checking your full username and password (TSB, Halifax, John Lewis), which is
+        effective in preventing keylogger attacks from gaining access (at least for several days).
+        Other banks (Santander, RBS) implement this to the detriment of their security.
         <br />
         <br />
-        The reason Santander and RBS&apos;s approaches are so dangerous is that
-        asking for specific characters prevents them from securely hashing and
-        salting your credentials when they are stored. Instead, Santander and
-        RBS must store your credentials either in plain text, or using a
-        reversible encryption algorithm. Either way, if a hacker accessed their
-        database of login credentials they would be able to use them immediately
-        to log into any account. On the contrary, if they stole hashed and
-        salted credentials, it would take them several hundred years to decode
-        the data and actually log in to any account.
+        The reason Santander and RBS&apos;s approaches are so dangerous is that asking for specific
+        characters prevents them from securely hashing and salting your credentials when they are
+        stored. Instead, Santander and RBS must store your credentials either in plain text, or
+        using a reversible encryption algorithm. Either way, if a hacker accessed their database of
+        login credentials they would be able to use them immediately to log into any account. On the
+        contrary, if they stole hashed and salted credentials, it would take them several hundred
+        years to decode the data and actually log in to any account.
         <br />
         <br />
         <TextLink
@@ -70,19 +60,14 @@ const UkBankSecurity = props => {
           src={santanderPasswordEntry}
         />
         <br />
-        In addition to their insecure login procedure, Santander employs
-        Hollywood security in the form of a user-selected image and phrase which
-        is shown during login. The idea behind this is that if someone were to
-        create a fake copy of their website, your selected image and phrase
-        would be wrong. In reality, it would be simple for the attacker to
-        simply lift the user-specific information from the genuine site and
-        place it into their forged site.
+        In addition to their insecure login procedure, Santander employs Hollywood security in the
+        form of a user-selected image and phrase which is shown during login. The idea behind this
+        is that if someone were to create a fake copy of their website, your selected image and
+        phrase would be wrong. In reality, it would be simple for the attacker to simply lift the
+        user-specific information from the genuine site and place it into their forged site.
         <br />
         <br />
-        <TextLink
-          href="https://en.wikipedia.org/wiki/SiteKey#Weaknesses"
-          external
-        >
+        <TextLink href="https://en.wikipedia.org/wiki/SiteKey#Weaknesses" external>
           Find out more on Wikipedia →
         </TextLink>
         <br />
