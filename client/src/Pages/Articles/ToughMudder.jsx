906cc0573db40acc4bacc3e394896cbb23f4c516
George Gillams <georgegillams@skyscanner.net>
2018-02-19 14:28:27 +0000

    Finish TM blog

--- client/src/Pages/Articles/ToughMudder.jsx
+++ client/src/Pages/Articles/ToughMudder.jsx
@@ -31,20 +31,24 @@ const ToughMudder = props => {
           <TextLink external href="https://toughmudder.co.uk/">
             Tough Mudder{' '}
           </TextLink>
-          (or anything similar) until my brother suggested it. It sounded like
-          the sort of thing that I would completely steer clear of, but on my
-          21st Birthday, after a few pints in a brewery, I agreed. I thought it
-          couldn&apos;t be that bad if I had my 6ft 6" brother on side.
+          (or anything similar) until my{' '}
+          <TextLink external href="http://ricgillamsphotography.co.uk/about-me">
+            brother{' '}
+          </TextLink>
+          suggested it. It sounded like the sort of thing that I would
+          completely steer clear of, but on my 21st Birthday, after a few pints
+          in a brewery, I agreed. I thought it couldn&apos;t be that bad if I
+          had a 6ft 6" giant on-side.
         </SubSection>
         <SubSection name="Tough Mudder 1">
           Ric and I did some training together to try and prepare ourselves for
           the crazy event we'd signed up to. We ran for several hours,
           improvised obstactles from anything we could find (playgrounds, etc)
-          and pledged to not drink beer in the 24 hours leading up to the event
-          (something we both failed to stick to).
+          and pledged to not drink beer in the ~7 days~ ~24 hours~ 12 hours
+          leading up to the event (something we both failed to stick to).
           <br />
           <br />
-          Before we knew it, the day had arrived. We were slightly nervous, but
+          Before we knew it the day had arrived. We were slightly nervous, but
           had done enough training and eaten enough pasta sandwiches in the
           preceding week to feel ready(ish). We parked up, ate several packs of
           giant Maryland cookies, and headed for registration, bag-drop, and the
@@ -57,15 +61,17 @@ const ToughMudder = props => {
           And there's something really refreshing about that. Within a few pages
           of his book, Will Dean (Tough Mudder founder) says that one of his
           biggest frustrations after he completed his first marathon was his
-          friend's obsession with his finishing time. He didn't think his time
+          peers' obsession with his finishing time. He didn't think his time
           should be relevant. He believed that the achievement was in completing
           it. And now he's made a global event that's all about getting over the
-          finish line - however long it takes.
+          finish line - irrespective of how long it takes.
           <br />
           <br />
-          The sense of teamwork and comaraderie at Tough Mudder event is nothing
-          but exceptional. The half-pipe looks impossible until you spot a group
-          of people at the top with arms hanging down ready to catch you.
+          The sense of teamwork and comaraderie at the event was nothing but
+          exceptional. Everest 2.0 seemed impossible until I spotted a group of
+          large blokes at the top with arms hanging down ready to help. When
+          they singled me out to go next the dread I felt running up to the
+          obstacle just dissapeared and I was happily scaling the half-pipe.
           Several obstacles are only possible with teamwork, and you can be
           given a legup buy a randommer at any time.
           <br />
@@ -83,47 +89,50 @@ const ToughMudder = props => {
         </SubSection>
         <SubSection name="Tough Mudder 2 (Half)">WRITE </SubSection>
         <SubSection name="Tough Mudder 3">
-          Having successfully completed one Tough Mudder, I wanted to get my
-          mates involved. I asked my housemates, old and new, and someone I'd
-          been on 1 date with! We headed up north for the event which I assured
-          them would be 'easier than it looks'. Admittedly I was possibly
-          (definitely) lying (a bit)! As luck had it, I also had an exam
-          scheduled for the next day, so I headed up north, books in hand to
-          meet the team in a pub. Needless to say no work got done. We ate,
-          drank, and preparad ourselves for vistory the next day.
+          Having enjoyed a couple of Tough Mudder events I wanted to get my
+          mates involved. I asked my housemates, old and new to get involved,
+          and encouraged them to sign up by telling blatant lies about how it
+          it's "easier than it looks"! I also asked along someone I'd been on 1
+          date with. It can't have been that bad because she's now my Fiancé! We
+          headed up north for the event. As luck had it, I also had an exam
+          scheduled for the next day at 0900, so I was on the coach to
+          Nottingham, books in hand, to meet the team in a pub and ~do lots of
+          revision~ drink beer. We ate, drank, and preparad ourselves for
+          vistory the next day.
         </SubSection>
         <SubSection name="Tough Mudder 4">
           Roll on two weeks from Tough Mudder 3, and I was back in the warm-up
           pen for London West. With (most) of the original team (except for my
-          6ft 6" brother who had bailed). It wasn't ideal to be doing it without
-          him. We'd actually have to get over the walls ourselves instead of
-          being thrown, but we did it regardless. T-shirts, headbands and beer
-          in hand, we agreed to do it again the next year. My challenge wasn't
-          over for the day, though. I was due to be attending a ball in
-          Southampton that evening, so I found a train and got back to
-          Southampton double time. My food had been saved for me (I needed it).
-          3 courses and a pint of Red Bull later I was trying not to fall asleep
-          on the dodgems!
+          6ft 6" brother, Ric, who had bailed due to now working in Tokyo!). It
+          wasn't ideal to be doing it without him. We'd actually have to get
+          over the walls ourselves instead of being thrown, but we got through
+          it. T-shirts, headbands and beer in hand, we agreed to do it again the
+          next year. My challenge wasn't over for the day, though. I was due to
+          be attending a ball in Southampton that evening, so I found a train
+          and got back to Southampton double time. The rest of my table had
+          insisted that my food could not be taken away, so when I arrived 3
+          courses and a pint of Red Bull were waiting. The dodgems made me feel
+          pretty shaken up. Who has dodgems at a ball!?
         </SubSection>
         <SubSection name="Tough Mudder 5">
-          A year later, the 5th Tough Mudder was coming round. Although Ric will
-          probably be with us this time, he's just spent 6 months in Japan
-          eating nothing but sushi. We therefore expect him to be a deadweight
-          we have to carry around the course! But, not all hope is lost. We have
-          an excellent inspirational quote from his boss on the other side of
-          the world:
+          Another year on and my 5th Tough Mudder is looming. Although Ric will
+          probably be with us this time, he has just spent 6 months in Japan
+          eating nothing but sushi. We therefore expect him to be nothing but an
+          additional deadweight we have to carry around the course! But, not all
+          hope is lost. We have an excellent inspirational quote from his boss
+          on the other side of the world:
           <br />
           <br />
           <Quote>
             "I generally find that there is a strong, negative correlation
             between how much fun something is and how prepared you are for it" -
-            my brother's boss
+            Ric's boss
           </Quote>
           <br />
           <br />
         </SubSection>
       </Section>
-      <Comments pageId={PAGE_ID} />
+      {/* <Comments pageId={PAGE_ID} /> */}
     </main>
   );
 };
