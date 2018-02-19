8cb80a28b3aeaef98504de6b7c9110e050a2dc86
George Gillams <georgegillams@skyscanner.net>
2018-02-19 08:12:49 +0000

    Fix hover

--- client/src/components/SubSection.jsx
+++ client/src/components/SubSection.jsx
@@ -27,15 +27,15 @@ const SubSection = props => {
     getClassName('typography__text'),
     getClassName('typography__text--subsection'),
   ];
+  if (hover) {
+    textClassNameFinal.push(getClassName('typography--hovering'));
+  }
   if (!noAnchor) {
     textClassNameFinal.push(getClassName('typography__text--with-anchor-link'));
   }
   if (light) {
     classNameFinal.push(getClassName('typography--light'));
     textClassNameFinal.push(getClassName('typography--light'));
-  }
-  if (hover) {
-    textClassNameFinal.push(getClassName('typography--hovering'));
   }
   if (link) {
     classNameFinal.push(getClassName('typography__link'));
