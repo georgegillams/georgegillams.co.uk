f86bd8ce8e7c5356c9ac8b9989b2f56951acba41
George Gillams <georgegillams@skyscanner.net>
2018-03-08 22:26:25 +0000

    Enable tag selections

--- client/src/Pages/CategoryPages/Articles.jsx
+++ client/src/Pages/CategoryPages/Articles.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import ArticleCard from '../../components/ArticleCard';
 import PageSwitchScroller from '../../components/PageSwitchScroller';
 import Tag, { TAG_TYPES } from '../../components/Tag';
@@ -18,83 +19,114 @@ import STYLES from '../pages.scss';
 
 const getClassName = className => STYLES[className] || 'UNKNOWN';
 
-const Articles = () => (
-  <div>
-    <PageSwitchScroller />
-    {/* <TagFilter className={getClassName('pages__card')} /> */}
-    {/* <ArticleCard
-      day={0}
-      month="NaN"
-      className={getClassName('pages__card')}
-      imageSrc={lrSm}
-      linkUrl="/blog/lightroom-workflow"
-      title="My Lightroom Workflow"
-    >
-      <Tag type={TAG_TYPES.photography} />
-    </ArticleCard>
-    <ArticleCard
-      light
-      day={0}
-      month="NaN"
-      className={getClassName('pages__card')}
-      imageBorder="cornflowerblue"
-      imageSrc={teapotGif}
-      fillImageSrc={teapot}
-      linkUrl="/blog/react-http-response-codes"
-      title="Sending a 418 in React.js"
-    >
-      <Tag type={TAG_TYPES.tech}  />
-    </ArticleCard>
-    <ArticleCard
-      day={0}
-      month="NaN"
-      className={getClassName('pages__card')}
-      imageSrc={rustSm}
-      linkUrl="/blog/week-of-rust"
-      title="My week of Rust"
-    >
-      <Tag type={TAG_TYPES.tech}  />
-    </ArticleCard>{' '} */}
-    <ArticleCard
-      light
-      imageBorder="darkorange"
-      day={19}
-      month="Feb"
-      className={getClassName('pages__card')}
-      fillImageSrc={toughMudder}
-      imageSrc={toughMudderSm}
-      linkUrl="/blog/tough-mudder"
-      title="Running 5 Tough Mudders"
-    >
-      <Tag type={TAG_TYPES.events} />
-    </ArticleCard>
-    <ArticleCard
-      backgroundImageClassName={getClassName('pages__nn-background-image')}
-      imageBorder="red"
-      day={8}
-      month="Dec"
-      className={getClassName('pages__card')}
-      fillImageSrc={netNeutrality}
-      imageSrc={netNeutralitySm}
-      linkUrl="/blog/net-neutrality"
-      title="My Take on Net Neutrality"
-    >
-      <Tag type={TAG_TYPES.tech} />
-    </ArticleCard>
-    <ArticleCard
-      light
-      day={23}
-      imageBorder="lightgray"
-      month="Dec"
-      className={getClassName('pages__card')}
-      fillImageSrc={vim}
-      imageSrc={vimSm}
-      linkUrl="/blog/vim"
-      title="Switching to Vim"
-    >
-      <Tag type={TAG_TYPES.tech} />
-    </ArticleCard>
-  </div>
-);
+const TagFilterRoutable = withRouter(TagFilter);
+
+class Articles extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedTags: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <PageSwitchScroller />
+        <TagFilterRoutable
+          selectedTags={this.state.selectedTags}
+          onSelectedTagsChanged={nst => {
+            this.setState({ selectedTags: nst });
+          }}
+          className={getClassName('pages__card')}
+        />
+        {/* <ArticleCard
+        day={0}
+        month="NaN"
+        className={getClassName('pages__card')}
+        imageSrc={lrSm}
+        linkUrl="/blog/lightroom-workflow"
+        title="My Lightroom Workflow"
+      >
+        <Tag type={TAG_TYPES.photography} />
+      </ArticleCard>
+      <ArticleCard
+        light
+        day={0}
+        month="NaN"
+        className={getClassName('pages__card')}
+        imageBorder="cornflowerblue"
+        imageSrc={teapotGif}
+        fillImageSrc={teapot}
+        linkUrl="/blog/react-http-response-codes"
+        title="Sending a 418 in React.js"
+      >
+        <Tag type={TAG_TYPES.tech}  />
+      </ArticleCard>
+      <ArticleCard
+        day={0}
+        month="NaN"
+        className={getClassName('pages__card')}
+        imageSrc={rustSm}
+        linkUrl="/blog/week-of-rust"
+        title="My week of Rust"
+      >
+        <Tag type={TAG_TYPES.tech}  />
+      </ArticleCard>{' '} */}
+        {(this.state.selectedTags.length === 0 ||
+          this.state.selectedTags.includes('events')) && (
+          <ArticleCard
+            light
+            imageBorder="darkorange"
+            day={19}
+            month="Feb"
+            className={getClassName('pages__card')}
+            fillImageSrc={toughMudder}
+            imageSrc={toughMudderSm}
+            linkUrl="/blog/tough-mudder"
+            title="Running 5 Tough Mudders"
+          >
+            <Tag type={TAG_TYPES.events} />
+          </ArticleCard>
+        )}
+        {(this.state.selectedTags.length === 0 ||
+          this.state.selectedTags.includes('tech')) && (
+          <ArticleCard
+            backgroundImageClassName={getClassName(
+              'pages__nn-background-image',
+            )}
+            imageBorder="red"
+            day={8}
+            month="Dec"
+            className={getClassName('pages__card')}
+            fillImageSrc={netNeutrality}
+            imageSrc={netNeutralitySm}
+            linkUrl="/blog/net-neutrality"
+            title="My Take on Net Neutrality"
+          >
+            <Tag type={TAG_TYPES.tech} />
+          </ArticleCard>
+        )}
+        {(this.state.selectedTags.length === 0 ||
+          this.state.selectedTags.includes('tech')) && (
+          <ArticleCard
+            light
+            day={23}
+            imageBorder="lightgray"
+            month="Dec"
+            className={getClassName('pages__card')}
+            fillImageSrc={vim}
+            imageSrc={vimSm}
+            linkUrl="/blog/vim"
+            title="Switching to Vim"
+          >
+            <Tag type={TAG_TYPES.tech} />
+          </ArticleCard>
+        )}
+      </div>
+    );
+  }
+}
 
 export default Articles;
