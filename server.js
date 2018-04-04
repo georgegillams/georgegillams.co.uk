d57ec0203cb902022777a79d2b2c9463ad0f938b
George Gillams <georgegillams@skyscanner.net>
2018-04-04 20:58:30 +0100

    Add robots.txt file

--- server.js
+++ server.js
@@ -44,6 +44,18 @@ const router = express.Router();
 
 app.use(staticFiles);
 
+router.get('/robots.txt', (req, res) => {
+  res.sendFile(path.join(__dirname, './server_content', 'robots.txt'), {
+    headers: { 'Content-Type': 'text/plain' },
+  });
+});
+
+router.get('/sitemap.xml', (req, res) => {
+  res.sendFile(path.join(__dirname, './server_content', 'sitemap.xml'), {
+    headers: { 'Content-Type': 'text/xml' },
+  });
+});
+
 router.get('/ontologies/2018/tv-listing-ontology', (req, res) => {
   res.sendFile(
     path.join(
