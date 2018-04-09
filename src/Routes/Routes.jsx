331f9051a4851d4836fcedda4d79ca70bfd886b4
George Gillams <georgegillams@skyscanner.net>
2018-04-09 10:40:08 +0100

    Add payments page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -28,6 +28,7 @@ import AboutMe from '../Pages/AboutMe';
 import ForOhFour from '../Pages/ForOhFour';
 import TeaPot from '../Pages/TeaPot';
 import Payments from '../Pages/Payments';
+import PaymentView from '../Pages/PaymentView';
 
 const redirects = [
   { from: '/articles/tough-mudder', to: '/blog/tough-mudder' },
@@ -81,6 +82,7 @@ const Routes = (
         />
         {/* <Route path="/work/bpk-component-demo" component={BpkComponentDemo} /> */}
         <Route path="/site-map" component={SiteMap} />
+        <Route path="/payments/view" component={PaymentView} />
         <Route path="/payments" component={Payments} />
         <Route path="/about" component={AboutMe} />
         <Route path="/contact" component={Contact} />
