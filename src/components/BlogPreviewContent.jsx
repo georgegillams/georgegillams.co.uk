6538a83c822242d04a3f2a7ecbd5cf327947ff45
George Gillams <georgegillams@skyscanner.net>
2018-04-24 13:07:22 +0100

    Add link processing

--- src/components/BlogPreviewContent.jsx
+++ src/components/BlogPreviewContent.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import TextLink from './TextLink';
 
 import STYLES from './article-date.scss';
 
 const getClassName = className => STYLES[className] || 'UNKNOWN';
 
-const BlogPreviewSection = props => {
+const MD_LINK_REGEX = /\[([^\[\]]*)\]\(([^\(\)]*)\)/gi;
+
+// This component works recursively. Each time it checks for a feature (such as a link, stikethrough etc)
+// At each stage, if it finds one it renders the appropriate component, passing the surrounding text to
+// another instance of BlogPreviewContent. if no such feature is found, the content is simply rendered.
+
+const BlogPreviewContent = props => {
   const {
     content,
     className,
@@ -30,6 +37,24 @@ const BlogPreviewSection = props => {
 
   const contentParts = content.split('\n');
 
+  const mdLink = content.split(MD_LINK_REGEX);
+  if (mdLink.length > 3) {
+    console.log(content);
+    console.log(content.split(MD_LINK_REGEX));
+    const preLinkText = mdLink.shift();
+    const linkText = mdLink.shift();
+    const linkRef = mdLink.shift();
+    const postLinkText = mdLink.join('');
+    return (
+      <div className={classNameFinal.join(' ')} {...rest}>
+        <TextLink inline href={linkRef}>
+          {linkText}
+        </TextLink>
+      </div>
+    );
+  }
+
+  // Otherwise we just return the block of text:
   return (
     <div className={classNameFinal.join(' ')} {...rest}>
       {contentParts.map(s => (
@@ -39,7 +64,7 @@ const BlogPreviewSection = props => {
   );
 };
 
-BlogPreviewSection.propTypes = {
+BlogPreviewContent.propTypes = {
   content: PropTypes.string.isRequired,
   className: PropTypes.string,
   elementClassName: PropTypes.string,
@@ -47,11 +72,11 @@ BlogPreviewSection.propTypes = {
   noAnchor: PropTypes.bool,
 };
 
-BlogPreviewSection.defaultProps = {
+BlogPreviewContent.defaultProps = {
   className: null,
   elementClassName: null,
   light: false,
   noAnchor: false,
 };
 
-export default BlogPreviewSection;
+export default BlogPreviewContent;
