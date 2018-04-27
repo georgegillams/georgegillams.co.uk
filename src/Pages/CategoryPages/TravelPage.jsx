233fb8fe621e10e2f6dc539c8686c8322c5324e9
George Gillams <georgegillams@skyscanner.net>
2018-04-27 19:20:43 +0100

    Implement server-side filtering

--- src/Pages/CategoryPages/TravelPage.jsx
+++ src/Pages/CategoryPages/TravelPage.jsx
@@ -20,20 +20,29 @@ class TravelPage extends Component {
     this.state = {
       selectedTags: ['travel'],
       travelBlogs: null,
+      refreshContinuously: true,
     };
   }
 
   componentDidMount() {
     const getTravelBlogs = () => {
-      DatabaseFunctions.getBlogs('', this.state.selectedTags, results => {
-        this.setState({
-          travelBlogs: results,
+      if (this.state.refreshContinuously) {
+        DatabaseFunctions.getBlogs('', this.state.selectedTags, results => {
+          this.setState({
+            travelBlogs: results,
+          });
         });
-      });
+      }
     };
 
     getTravelBlogs();
     setInterval(getTravelBlogs, 2000);
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      refreshContinuously: false,
+    });
   }
 
   render() {
@@ -47,7 +56,11 @@ class TravelPage extends Component {
               b =>
                 !b.blogShowInBlogsList ? null : (
                   <ArticleCard
-                    day={new Date(1000 * b.publishedTimestamp).getDate()}
+                    day={
+                      b.blogCardDate
+                        ? b.blogCardDate
+                        : new Date(1000 * b.publishedTimestamp).getDate()
+                    }
                     month={new Date(1000 * b.publishedTimestamp).getMonth()}
                     className={getClassName('pages__card')}
                     fillImageSrc={b.blogHeroImage}
