303244e50d847eb2610699d9208aa0812a939a0f
George Gillams <georgegillams@skyscanner.net>
2018-04-27 17:28:34 +0100

    Blog organisation

--- src/Pages/CategoryPages/TravelPage.jsx
+++ src/Pages/CategoryPages/TravelPage.jsx
@@ -1,67 +1,82 @@
-import React from 'react';
-import LicenseInfo from '../../components/LicenseInfo';
-import PageSwitchScroller from '../../components/PageSwitchScroller';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import ArticleCard from '../../components/ArticleCard';
-import iceland from '../Travel/images/iceland2.jpg';
-import longleat from '../Travel/images/longleatLight.jpg';
-import munich from '../Travel/images/munich.jpg';
-import mickeyMouse from '../Travel/images/mickeyMouse.jpg';
-import serreChevalier from '../Travel/images/serreChevalier.jpg';
+import PageSwitchScroller from '../../components/PageSwitchScroller';
+import Loading from '../../components/Loading';
+import Tag from '../../components/Tag';
+import TagFilter from '../../components/TagFilter';
+import DatabaseFunctions from '../../DatabaseFunctions';
 
 import STYLES from '../pages.scss';
 
 const getClassName = className => STYLES[className] || 'UNKNOWN';
 
-const TravelPage = () => (
-  <div style={{ width: '100%' }}>
-    {' '}
-    <PageSwitchScroller />
-    <ArticleCard
-      day="01-07"
-      month="Jan"
-      className={getClassName('pages__card')}
-      imageSrc={iceland}
-      linkUrl="/travel/iceland-2018"
-      title="Iceland 2018"
-      autoTallLayout
-    />
-    <ArticleCard
-      day={27}
-      month="Dec"
-      className={getClassName('pages__card')}
-      imageSrc={longleat}
-      linkUrl="/travel/longleat-2017"
-      title="Longleat 2017"
-      autoTallLayout
-    />
-    <ArticleCard
-      day="15-20"
-      month="Dec"
-      className={getClassName('pages__card')}
-      imageSrc={munich}
-      linkUrl="/travel/munich-2017"
-      title="Munich 2017"
-      autoTallLayout
-    />
-    <ArticleCard
-      day="19-23"
-      month="Jun"
-      className={getClassName('pages__card')}
-      imageSrc={mickeyMouse}
-      linkUrl="/travel/disneyland-2017"
-      title="Disneyland 2017"
-      autoTallLayout
-    />
-    <ArticleCard
-      day="26-02"
-      month="Mar"
-      className={getClassName('pages__card')}
-      imageSrc={serreChevalier}
-      linkUrl="/travel/serre-chevalier-2017"
-      title="Serre Chevalier 2017"
-      autoTallLayout
-    />
-  </div>
-);
+const TagFilterRoutable = withRouter(TagFilter);
+
+class TravelPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedTags: [],
+      travelBlogs: null,
+    };
+  }
+
+  componentDidMount() {
+    const getTravelBlogs = () => {
+      DatabaseFunctions.getBlogs('', results => {
+        this.setState({
+          travelBlogs: results,
+        });
+      });
+    };
+
+    getTravelBlogs();
+    setInterval(getTravelBlogs, 2000);
+  }
+
+  render() {
+    return (
+      <div style={{ width: '100%' }}>
+        <PageSwitchScroller />
+        <TagFilterRoutable
+          selectedTags={this.state.selectedTags}
+          onSelectedTagsChanged={nst => {
+            this.setState({ selectedTags: nst });
+          }}
+          className={getClassName('pages__tag-filter')}
+        />
+        {!this.state.travelBlogs && <Loading />}
+        {this.state.travelBlogs && (
+          <div>
+            {this.state.travelBlogs.map(
+              b =>
+                !b.blogShowInBlogsList ? null : (
+                  <ArticleCard
+                    day={new Date(1000 * b.publishedTimestamp).getDate()}
+                    month={new Date(1000 * b.publishedTimestamp).getMonth()}
+                    className={getClassName('pages__card')}
+                    fillImageSrc={b.blogHeroImage}
+                    imageSrc={b.blogImage}
+                    linkUrl={`/blog/view?id=${b.blogId}`}
+                    title={b.blogName}
+                    imageBorder={
+                      b.blogImageBorderColor ? b.blogImageBorderColor : null
+                    }
+                    bannerColor={b.blogBannerColor ? b.blogBannerColor : null}
+                    autoTallLayout
+                    light={b.blogCardLight}
+                  >
+                    {b && b.blogTags && b.blogTags.map(t => <Tag type={t} />)}
+                  </ArticleCard>
+                ),
+            )}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default TravelPage;
