5589ccb3ad8679c7577b08ab998c7b2bbe7ae057
George Gillams <georgegillams@skyscanner.net>
2018-04-27 17:28:34 +0100

    Blog organisation

--- src/Pages/CategoryPages/BlogsPage.jsx
+++ src/Pages/CategoryPages/BlogsPage.jsx
@@ -1,27 +1,11 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import { BpkExtraLargeSpinner, SPINNER_TYPES } from 'bpk-component-spinner';
-import Funnies from 'funnies';
 import ArticleCard from '../../components/ArticleCard';
 import PageSwitchScroller from '../../components/PageSwitchScroller';
-import Tag, { TAG_TYPES } from '../../components/Tag';
+import Loading from '../../components/Loading';
+import Tag from '../../components/Tag';
 import TagFilter from '../../components/TagFilter';
-import Section from '../../components/Section';
-import SubSection from '../../components/SubSection';
-import netNeutrality from '../Articles/images/spinner.gif';
-import netNeutralitySm from '../Articles/images/netNeutralitySm.jpg';
-import vim from '../Articles/images/vim.jpg';
-import vimSm from '../Articles/images/vimSm.png';
-import toughMudder from '../Articles/images/toughMudder.jpg';
-import toughMudderSm from '../Articles/images/toughMudderSm.jpg';
-import rustSm from '../Articles/images/rustSm.jpg';
-import lrSm from '../Articles/images/lrSm.jpg';
-import teapotGif from '../Articles/images/teapot.gif';
-import teapot from '../Articles/images/teapot.jpg';
-import HelperFunctions from '../../HelperFunctions';
 import DatabaseFunctions from '../../DatabaseFunctions';
-
-const funnies = new Funnies();
 
 import STYLES from '../pages.scss';
 
@@ -63,18 +47,7 @@ class BlogsPage extends Component {
           }}
           className={getClassName('pages__tag-filter')}
         />
-        {!this.state.blogs && (
-          <SubSection
-            noAnchor
-            style={{ textAlign: 'center' }}
-            name="Loading..."
-          >
-            {funnies.message()}
-            <br />
-            <br />
-            <BpkExtraLargeSpinner large type={SPINNER_TYPES.dark} />
-          </SubSection>
-        )}
+        {!this.state.blogs && <Loading />}
         {this.state.blogs && (
           <div>
             {this.state.blogs.map(
