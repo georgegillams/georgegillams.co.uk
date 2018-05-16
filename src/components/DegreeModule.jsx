8cb803ea6fbbf1a1c91c6e227371cb4a31c40f02
George Gillams <georgegillams@skyscanner.net>
2018-05-16 17:06:49 +0100

    Add fonal degree marks

--- src/components/DegreeModule.jsx
+++ src/components/DegreeModule.jsx
@@ -10,33 +10,53 @@ import STYLES from './degree-module.scss';
 const getClassName = className => STYLES[className] || 'UNKNOWN';
 
 const DegreeModule = props => {
-  const { name, percentage, filled, className, ...rest } = props;
+  const { name, predicted, percentage, filled, className, ...rest } = props;
 
   const classNameFinal = [getClassName('degree-module')];
   if (className) {
     classNameFinal.push(className);
   }
 
+  let progress = (
+    <BpkProgress
+      style={{ position: 'absolute', width: '100%', top: '-.2rem' }}
+      small
+      min={0}
+      max={100}
+      value={filled ? percentage || 0 : 0}
+      aria-label={`Degree percentage - ${
+        percentage ? `${percentage}%` : 'pending'
+      }`}
+    />
+  );
+
+  if (predicted) {
+    progress = (
+      <BpkThemeProvider
+        theme={{ progressBarFillColor: '#e02626' }}
+        themeAttributes={[...progressThemeAttributes]}
+      >
+        {progress}
+      </BpkThemeProvider>
+    );
+  } else {
+    progress = (
+      <BpkThemeProvider
+        theme={{ progressBarFillColor: '#44aeff' }}
+        themeAttributes={[...progressThemeAttributes]}
+      >
+        {progress}
+      </BpkThemeProvider>
+    );
+  }
+
   return (
     <span className={classNameFinal.join(' ')} {...rest}>
       <span className={getClassName('degree-module--module-name')}>{name}</span>
       <div className={getClassName('degree-module--module-bar')}>
-        <BpkThemeProvider
-          theme={{ progressBarFillColor: '#44aeff' }}
-          themeAttributes={[...progressThemeAttributes]}
-        >
-          <BpkProgress
-            style={{ position: 'absolute', width: '100%', top: '-.2rem' }}
-            small
-            min={0}
-            max={100}
-            value={filled ? percentage || 0 : 0}
-            aria-label={`Degree percentage - ${
-              percentage ? `${percentage}%` : 'pending'
-            }`}
-          />{' '}
-        </BpkThemeProvider>
-        <div className={getClassName('degree-module--first-marker')} />
+        {progress}
+        <div className={getClassName('degree-module--21-marker')} />
+        {/* <div className={getClassName('degree-module--first-marker')} /> */}
       </div>
       <span
         className={getClassName('degree-module--percentage')}
@@ -49,12 +69,14 @@ const DegreeModule = props => {
 DegreeModule.propTypes = {
   name: PropTypes.string.isRequired,
   percentage: PropTypes.number,
+  predicted: PropTypes.bool,
   filled: PropTypes.bool,
   className: PropTypes.string,
 };
 
 DegreeModule.defaultProps = {
   filled: false,
+  predicted: false,
   className: null,
   percentage: null,
 };
