8f85c3db5a8a9490bad8f8c7a0e3108ba9c51b63
George Gillams <georgegillams@skyscanner.net>
2018-06-03 19:48:15 +0100

    Add typography page content

--- src/Pages/Design/DesignTypography.jsx
+++ src/Pages/Design/DesignTypography.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
-import { BpkSpinner, SPINNER_TYPES } from 'bpk-component-spinner';
-import BpkInput from 'bpk-component-input';
+import withLazyLoading from '../../components/withLazyLoading';
 import Section from '../../components/Section';
 import TextLink from '../../components/TextLink';
 import SubSection from '../../components/SubSection';
+import ArticleDate from '../../components/ArticleDate';
 import Button from '../../components/Button';
-import DatabaseFunctions from '../../DatabaseFunctions';
+import Code from '../../components/Code';
+import CodeInline from '../../components/CodeInline';
+import CodeBashArrow from '../../components/CodeBashArrow';
+import Logo from '../../components/Logo';
+import Loading from '../../components/Loading';
+import Quote from '../../components/Quote';
+import RedirectNotice from '../../components/RedirectNotice';
+import Tag from '../../components/Tag';
+import NotificationComp from '../../components/NotificationComp';
+import AnimatedContent from '../../components/AnimatedContent';
+import bpkDemo from '../../components/bpk-component-demo/src/BpkDemo';
 
 import STYLES from '../pages.scss';
 
 const getClassName = className => STYLES[className] || 'UNKNOWN';
+
+const documentIfExists = typeof window !== 'undefined' ? document : null;
+const LlAnimatedContent = withLazyLoading(AnimatedContent, documentIfExists);
+
+const SectionDemo = bpkDemo(Section, 'Section', 'na', {
+  name: 'Section name',
+  children: 'Some section content',
+});
+const SubSectionDemo = bpkDemo(SubSection, 'SubSection', 'na', {
+  name: 'Sub-section name',
+  children: 'Some sub-section content',
+});
 
 class DesignTypography extends React.Component {
   constructor(props) {
@@ -26,7 +48,12 @@ class DesignTypography extends React.Component {
 
     return (
       <Section name="Typography">
-        <SubSection noAnchor name="Coming soon..." />
+        <SubSection noAnchor name="Section">
+          <SectionDemo />
+        </SubSection>
+        <SubSection noAnchor name="Subsection">
+          <SubSectionDemo />
+        </SubSection>
       </Section>
     );
   }
