69d6fafbc60ff216f114e6e6cf8c0c78751c932d
George Gillams <georgegillams@skyscanner.net>
2018-06-22 18:09:33 +0100

    Add typography

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.scss';
 import ArticleDate from './ArticleDate';
+import Section from './Section';
+import SubSection from './SubSection';
 import reactLogo from 'assets/img/react_logo.svg';
 
 class App extends React.PureComponent {
@@ -9,6 +11,11 @@ class App extends React.PureComponent {
       <div className="app">
         <h1>Hello World!</h1>
         <p>Foo to the bar</p>
+        <Section name="Running 5 Tough Mudders">
+          <SubSection noAnchor name="Why I did it">
+            Hellowowoww
+          </SubSection>
+        </Section>
         <ArticleDate date={Date.now()} />
         <img src={reactLogo} height="480" />
       </div>
