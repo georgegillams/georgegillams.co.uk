e472e5ef4bb41a368d1341aa78a6237f8eba51aa
George Gillams <georgegillams@skyscanner.net>
2018-06-22 17:16:51 +0100

    Add date component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import "./App.scss";
-import reactLogo from "assets/img/react_logo.svg";
+import React from 'react';
+import './App.scss';
+import ArticleDate from './ArticleDate';
+import reactLogo from 'assets/img/react_logo.svg';
 
 class App extends React.PureComponent {
   render() {
@@ -8,6 +9,7 @@ class App extends React.PureComponent {
       <div className="app">
         <h1>Hello World!</h1>
         <p>Foo to the bar</p>
+        <ArticleDate date={Date.now()} />
         <img src={reactLogo} height="480" />
       </div>
     );
---
+++ src/components/ArticleDate.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+// import BpkText from 'bpk-component-text';
+
+import STYLES from './article-date.scss';
+
+const getClassName = className => STYLES[className] || 'UNKNOWN';
+
+const ArticleDate = props => {
+  const { date, noPadding, className } = props;
+
+  const classNameFinal = [getClassName('article-date__date')];
+  if (noPadding) {
+    classNameFinal.push(getClassName('article-date__date--no-padding'));
+  }
+  if (className) {
+    classNameFinal.push(className);
+  }
+
+  return (
+    <span
+      tagName="h3"
+      textStyle="base"
+      style={{ fontStyle: 'italic' }}
+      className={classNameFinal.join(' ')}
+    >
+      Published {date.toString()}
+    </span>
+  );
+};
+
+ArticleDate.propTypes = {
+  date: PropTypes.number.isRequired,
+  className: PropTypes.string,
+  noPadding: PropTypes.bool,
+};
+
+ArticleDate.defaultProps = {
+  className: null,
+  noPadding: false,
+};
+
+export default ArticleDate;
