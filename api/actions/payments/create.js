d8ef9d485673eac9a1d0942d445eae5fcd046739
George Gillams <george.gillams@skyscanner.net>
2018-09-12 19:22:59 +0100

    Add server-side regex validation

--- api/actions/payments/create.js
+++ api/actions/payments/create.js
@@ -1,13 +1,20 @@
-import { datumCreate } from "../datum";
-import authentication from "../../utils/authentication";
+import { datumCreate } from '../datum';
+import authentication from '../../utils/authentication';
+import reqSecure from '../../utils/reqSecure';
+import paymentsAllowedAttributes from './paymentsAllowedAttributes';
 
 export default function create(req) {
   return new Promise((resolve, reject) => {
     authentication(req).then(
       user => {
-        resolve(datumCreate({ redisKey: "payments", user: user }, req));
+        resolve(
+          datumCreate(
+            { redisKey: 'payments', user: user },
+            reqSecure(req, paymentsAllowedAttributes),
+          ),
+        );
       },
-      err => reject(err)
+      err => reject(err),
     );
   });
 }
