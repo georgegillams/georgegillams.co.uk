75da2d4de590524e443a0e8b6642867b23b1dec0
George Gillams <george.gillams@skyscanner.net>
2018-09-13 17:50:30 +0100

    Secure query parameters too

--- api/utils/reqSecure.js
+++ api/utils/reqSecure.js
@@ -1,29 +1,37 @@
-import { INT_REGEX, STRING_REGEX } from '../../src/utils/constants';
+import { INT_REGEX, ID_REGEX, PASSWORD_REGEX } from '../../src/utils/constants';
 
 const standardAttributes = [
   { attribute: 'deleted', pattern: 'BOOL' },
-  { attribute: 'id', pattern: STRING_REGEX },
+  { attribute: 'id', pattern: ID_REGEX },
+  { attribute: 'apiKey', pattern: PASSWORD_REGEX },
+  { attribute: 'sessionKey', pattern: ID_REGEX },
   { attribute: 'timestamp', pattern: INT_REGEX },
 ];
 
-export default function reqSecure(req, allowedAttributes) {
-  const newBody = {};
-  const allAllowedAttributes = [...allowedAttributes, ...standardAttributes];
+function generateNewComponent(component, req, allAllowedAttributes) {
+  const newComponent = {};
   for (let i = 0; i < allAllowedAttributes.length; i += 1) {
     const name = allAllowedAttributes[i].attribute;
     const pattern = allAllowedAttributes[i].pattern;
-    if (req.body[name]) {
+    if (req[component][name]) {
       if (pattern === 'BOOL') {
-        newBody[name] = !!req.body[name];
+        newComponent[name] = !!req[component][name];
       } else {
         // pattern is a regex:
-        if (pattern && req.body[name].toString().match(pattern)) {
-          newBody[name] = req.body[name];
+        if (pattern && req[component][name].toString().match(pattern)) {
+          newComponent[name] = req[component][name];
         }
       }
     }
   }
+  return newComponent;
+}
 
-  req.body = newBody;
+export default function reqSecure(req, allowedAttributes) {
+  const allAllowedAttributes = [...allowedAttributes, ...standardAttributes];
+
+  req.body = generateNewComponent('body', req, allAllowedAttributes);
+  req.query = generateNewComponent('query', req, allAllowedAttributes);
+  req.headers = generateNewComponent('headers', req, allAllowedAttributes);
   return req;
 }
