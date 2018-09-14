835a068a958f36e26ba80084cf5525d7f0c907ed
George Gillams <george.gillams@skyscanner.net>
2018-09-14 18:12:27 +0100

    Remove broken tag filter

--- src/containers/Blogs/Blogs.js
+++ src/containers/Blogs/Blogs.js
@@ -3,14 +3,14 @@ import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 import {
   isLoaded as isBlogsLoaded,
-  load as loadBlogs
+  load as loadBlogs,
 } from 'redux/modules/blogs';
 import { bindActionCreators } from 'redux';
 import { asyncConnect } from 'redux-async-connect';
 import { ArticleCard, BlogsList, Loading, TagFilter, Tag } from 'components';
 import {
   NON_EMOJI_REGEX,
-  CHECK_FOR_NEW_CONTENT_INTERVAL
+  CHECK_FOR_NEW_CONTENT_INTERVAL,
 } from '../../utils/constants';
 import { cssModules } from 'bpk-react-utils';
 
@@ -28,26 +28,26 @@ const getClassName = cssModules(STYLES);
       }
 
       return Promise.all(promises);
-    }
-  }
+    },
+  },
 ])
 @connect(
   state => ({
     newDataAvailable: state.sessions.newDataAvailable,
-    blogs: state.blogs ? state.blogs.data : null
+    blogs: state.blogs ? state.blogs.data : null,
   }),
-  dispatch => bindActionCreators({ loadBlogs }, dispatch)
+  dispatch => bindActionCreators({ loadBlogs }, dispatch),
 )
 export default class Blogs extends Component {
   static propTypes = {
     newDataAvailable: PropTypes.bool.isRequired,
     blogs: PropTypes.arrayOf(PropTypes.object),
     loadBlogs: PropTypes.func.isRequired,
-    className: PropTypes.string
+    className: PropTypes.string,
   };
 
   static defaultProps = {
-    className: null
+    className: null,
   };
 
   constructor(props) {
@@ -59,7 +59,7 @@ export default class Blogs extends Component {
   componentDidMount = () => {
     this.interval = setInterval(
       this.reloadBlogsIfNecessary,
-      CHECK_FOR_NEW_CONTENT_INTERVAL
+      CHECK_FOR_NEW_CONTENT_INTERVAL,
     );
   };
 
@@ -89,13 +89,13 @@ export default class Blogs extends Component {
     return (
       <div className={outerClassNameFinal.join(' ')} {...rest}>
         <Helmet title="Travel" />
-        <TagFilter
+        {/* <TagFilter
           selectedTags={this.state.selectedTags}
           onSelectedTagsChanged={nst => {
             this.setState({ selectedTags: nst });
           }}
           className={getClassName('pages__tag-filter')}
-        />
+        /> */}
         {!blogs && <Loading />}
         {blogs && (
           <BlogsList
