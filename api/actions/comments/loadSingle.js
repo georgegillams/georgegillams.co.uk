fde60f378a32f2155007fc8de942d72be7b0b8f4
George Gillams <george.gillams@skyscanner.net>
2018-09-23 11:54:01 +0100

    Regex checks for comments

--- api/actions/comments/loadSingle.js
+++ api/actions/comments/loadSingle.js
@@ -1,28 +1,31 @@
-import { datumLoadSingle } from "../datum";
-import authentication from "../../utils/authentication";
+import { datumLoadSingle } from '../datum';
+import authentication from '../../utils/authentication';
+import reqSecure from '../../utils/reqSecure';
+import commentsAllowedAttributes from './commentsAllowedAttributes';
 
 export default function loadSingle(req) {
+  const reqSecured = reqSecure(req, commentsAllowedAttributes);
   return new Promise((resolve, reject) => {
-    authentication(req).then(
+    authentication(reqSecured).then(
       user => {
         if (user && user.admin) {
           resolve(
             datumLoadSingle({
-              redisKey: "comments",
+              redisKey: 'comments',
               includeDeleted: true,
-              filter: ar => ar.id === req.query.id
-            })
+              filter: ar => ar.id === reqSecured.query.id,
+            }),
           );
         } else {
           resolve(
             datumLoadSingle({
-              redisKey: "comments",
-              filter: ar => ar.id === req.query.id
-            })
+              redisKey: 'comments',
+              filter: ar => ar.id === reqSecured.query.id,
+            }),
           );
         }
       },
-      err => reject(err)
+      err => reject(err),
     );
   });
 }
