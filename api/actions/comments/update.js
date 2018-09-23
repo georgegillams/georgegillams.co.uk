bea7fcdab8e36b9851e48197891c3bc760bed5f0
George Gillams <george.gillams@skyscanner.net>
2018-09-23 11:54:01 +0100

    Regex checks for comments

--- api/actions/comments/update.js
+++ api/actions/comments/update.js
@@ -1,24 +1,27 @@
-import { datumUpdate } from "../datum";
-import authentication from "../../utils/authentication";
-import { userOwnsResource } from "../../utils/userOwnsResource";
-import { UNAUTHORISED_WRITE } from "../../../src/utils/constants";
+import { datumUpdate } from '../datum';
+import authentication from '../../utils/authentication';
+import { userOwnsResource } from '../../utils/userOwnsResource';
+import { UNAUTHORISED_WRITE } from '../../../src/utils/constants';
+import reqSecure from '../../utils/reqSecure';
+import commentsAllowedAttributes from './commentsAllowedAttributes';
 
 export default function update(req) {
+  const reqSecured = reqSecure(req, commentsAllowedAttributes);
   return new Promise((resolve, reject) => {
-    authentication(req).then(
+    authentication(reqSecured).then(
       user => {
-        userOwnsResource("comments", req.body.id, user).then(
+        userOwnsResource('comments', reqSecured.body.id, user).then(
           userOwnsResourceResult => {
             // Users should be able to update comments that they own
             if (user && (user.admin || userOwnsResourceResult)) {
-              resolve(datumUpdate({ redisKey: "comments" }, req));
+              resolve(datumUpdate({ redisKey: 'comments' }, reqSecured));
             } else {
               reject(UNAUTHORISED_WRITE);
             }
-          }
+          },
         );
       },
-      err => reject(err)
+      err => reject(err),
     );
   });
 }
