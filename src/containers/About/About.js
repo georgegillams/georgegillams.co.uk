258ec01ef8521c2a2157b8c4e4c4d36bb6ad454a
George Gillams <george.gillams@skyscanner.net>
2018-11-04 15:35:46 +0000

    Fix about page

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -14,38 +14,38 @@ export default class Contact extends Component {
     return (
       <div
         className={`${getClassName('pages__container')} ${getClassName(
-          'pages__container--centered'
+          'pages__container--centered',
         )}`}
       >
         <Helmet title="About" />
         <Section name="Hi! I'm George.">
-          I&apos;m an open-source software engineer at{" "}
+          I&apos;m an open-source software engineer at{' '}
           <TextLink external href="https://www.skyscanner.net/">
-            Skyscanner{" "}
+            Skyscanner{' '}
           </TextLink>
-          , passionate about design, <TextLink href="/travel">travel</TextLink>{" "}
-          and photography, and have an unsatisfiable hunger for learning. When
+          , passionate about design, <TextLink href="/travel">travel</TextLink>{' '}
+          and photography. I have an unsatisfiable hunger for learning. When
           I&apos;m not internationally photobombing unsuspecting strangers
           I&apos;m based somewhere between Southampton and London.
           <br />
           <br />I enjoy getting outdoors and trying new things, and completed my
           5th <TextLink href="/blog/tough-mudder">Tough Mudder</TextLink> in May
           2018. I also try to squeeze in as much volunteering as I can, so you
-          may find me dressed as a{" "}
+          may find me dressed as a{' '}
           <TextLink external href="https://helpforheroes.org.uk/">
-            Help for Heroes{" "}
-          </TextLink>{" "}
-          bear, reviewing content for{" "}
+            Help for Heroes{' '}
+          </TextLink>{' '}
+          bear, reviewing content for{' '}
           <TextLink external href="https://b-eat.co.uk/">
-            Beat{" "}
+            Beat{' '}
           </TextLink>
-          , or submitting PR's to{" "}
+          , or submitting PR's to{' '}
           <TextLink external href="https://github.com/mozilla">
-            Mozilla &apos;s open-source{" "}
+            Mozilla &apos;s open-source{' '}
           </TextLink>
           . I look forward to doing more of all that when I finish my Masters.
           <br />
-          <br />I recently got engaged to my wonderful fiancée in{" "}
+          <br />I recently got engaged to my wonderful fiancée in{' '}
           <TextLink href="/travel/iceland-2018">Iceland</TextLink> 💍, and live
           with her and Tigger the cat.
           <br />
@@ -57,7 +57,7 @@ export default class Contact extends Component {
                 'https://i.imgur.com/EPfA1yI.png', // engaged
                 'https://i.imgur.com/eldzRhk.jpg', // us
                 'https://i.imgur.com/n8JZuqv.jpg', // tiggs
-                'https://i.imgur.com/mvvDmiL.jpg' // tm
+                'https://i.imgur.com/mvvDmiL.jpg', // tm
               ]}
             />
           </div>
@@ -69,7 +69,7 @@ export default class Contact extends Component {
           browser.
           <br />I use this site to experiment with things, share stuff I've
           figured out, and allow people to reach-out to me. So if you're
-          interested in anything I do,{" "}
+          interested in anything I do,{' '}
           <TextLink href="/contact">get in touch</TextLink>!
         </Section>
       </div>
