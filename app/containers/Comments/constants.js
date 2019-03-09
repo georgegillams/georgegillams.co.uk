d303ebe475e9e590693cfd91cfb4313c53a5e668
George Gillams <george.gillams@skyscanner.net>
2019-03-09 17:21:40 +0000

    Enable comment update and delete

--- app/containers/Comments/constants.js
+++ app/containers/Comments/constants.js
@@ -7,3 +7,11 @@ export const CREATE_COMMENT = 'boilerplate/Comments/CREATE_COMMENT';
 export const CREATE_COMMENT_ERROR = 'boilerplate/Comments/CREATE_COMMENT_ERROR';
 export const CREATE_COMMENT_SUCCESS =
   'boilerplate/Comments/CREATE_COMMENT_SUCCESS';
+export const UPDATE_COMMENT = 'boilerplate/Comments/UPDATE_COMMENT';
+export const UPDATE_COMMENT_ERROR = 'boilerplate/Comments/UPDATE_COMMENT_ERROR';
+export const UPDATE_COMMENT_SUCCESS =
+  'boilerplate/Comments/UPDATE_COMMENT_SUCCESS';
+export const DELETE_COMMENT = 'boilerplate/Comments/DELETE_COMMENT';
+export const DELETE_COMMENT_ERROR = 'boilerplate/Comments/DELETE_COMMENT_ERROR';
+export const DELETE_COMMENT_SUCCESS =
+  'boilerplate/Comments/DELETE_COMMENT_SUCCESS';
