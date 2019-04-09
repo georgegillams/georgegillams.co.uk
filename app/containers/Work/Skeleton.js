d40d3f1ed9e774f9190b1bf19abae071e20743b9
George Gillams <george.gillams@skyscanner.net>
2019-04-09 17:29:27 +0100

    Make prose pages narrower

--- app/containers/Work/Skeleton.js
+++ app/containers/Work/Skeleton.js
@@ -6,12 +6,17 @@ import {
   SectionSkeleton,
 } from 'components/Skeletons';
 import TicketStatusSkeleton from 'containers/TicketStatus/Skeleton';
-import STYLES from 'containers/pages.scss'; import {cssModules} from 'bpk-react-utils'; const getClassName = cssModules(STYLES);
+import STYLES from 'containers/pages.scss';
+import { cssModules } from 'bpk-react-utils';
+const getClassName = cssModules(STYLES);
 
 const LoginFormSkeleton = props => {
   const { className, ...rest } = props; // eslint-disable-line no-shadow
 
-  const outerClassNameFinal = [getClassName('pages__container')];
+  const outerClassNameFinal = [
+    getClassName('pages__container'),
+    getClassName('pages__container--prose'),
+  ];
 
   if (className) {
     outerClassNameFinal.push(className);
