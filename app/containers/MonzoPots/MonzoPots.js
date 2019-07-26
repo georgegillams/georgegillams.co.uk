b4713b8f9c7591b6bdd809a00238f543af873ba3
George Gillams <george.gillams@skyscanner.net>
2019-07-26 20:06:58 +0100

    Display data

--- app/containers/MonzoPots/MonzoPots.js
+++ app/containers/MonzoPots/MonzoPots.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import BpkImage, {
@@ -7,13 +7,14 @@ import BpkImage, {
 } from 'bpk-component-image';
 import BpkInput, { INPUT_TYPES, CLEAR_BUTTON_MODES } from 'bpk-component-input';
 import { cssModules } from 'bpk-react-utils';
+import { associate } from 'helpers/objects';
 
 import Skeleton from './Skeleton';
 
 import HelperFunctions from 'helpers/HelperFunctions';
 import MoneyPot from 'components/MoneyPot';
 import GGButton from 'components/GGButton';
-import { Section } from 'components/Typography';
+import { Section, SubSection } from 'components/Typography';
 import { LoadingCover } from 'components/Auth';
 import STYLES from 'containers/pages.scss';
 
@@ -26,7 +27,6 @@ export default class MonzoPots extends React.Component {
   };
 
   render() {
-    console.log(`this.props`, this.props);
     const {
       loadMonzo,
       loadTransactions,
@@ -48,12 +48,24 @@ export default class MonzoPots extends React.Component {
       outerClassNameFinal.push(className);
     }
 
+    let monzoPotDisplayData = monzoPots;
+    if (monzoPots && monzoPots.map && transactions && transactions.map) {
+      monzoPotDisplayData = associate(
+        monzoPots,
+        transactions,
+        'name',
+        'name',
+        'transactionalData',
+      );
+      console.log(`monzoPotDisplayData`, monzoPotDisplayData);
+    }
+
     return (
       <div className={outerClassNameFinal.join(' ')} {...rest}>
         <Helmet title="My monzo pots" />
         <Section>
           <Section name="Monzo pot tracking 💳">
-            {!monzoPots && (
+            {!monzoPotDisplayData && (
               <BpkInput
                 id="password"
                 className={getClassName('pages__component')}
@@ -69,7 +81,7 @@ export default class MonzoPots extends React.Component {
                 onClear={() => this.setState({ password: '' })}
               />
             )}
-            {monzoPots && monzoPots.map && (
+            {monzoPotDisplayData && monzoPotDisplayData.map && (
               <GGButton
                 onClick={() => {
                   this.loadPotData(password);
@@ -80,19 +92,37 @@ export default class MonzoPots extends React.Component {
             )}
             <br />
             <br />
-            {monzoPots &&
-              monzoPots.map &&
-              monzoPots.map((pot, index) => (
-                <MoneyPot
-                  markerPosition={pot.percentageTimeElapsed}
-                  className={getClassName('pages__degree-module')}
-                  name={pot.name}
-                  shortfall={pot.shortfall}
-                  balance={pot.balance}
-                  goalAmount={pot.goalAmount}
-                  percentage={pot.percentageComplete}
-                  filled
-                />
+            {monzoPotDisplayData &&
+              monzoPotDisplayData.map &&
+              monzoPotDisplayData.map(pot => (
+                <Fragment>
+                  <MoneyPot
+                    markerPosition={pot.percentageTimeElapsed}
+                    className={getClassName('pages__degree-module')}
+                    name={pot.name}
+                    shortfall={pot.shortfall}
+                    balance={pot.balance}
+                    goalAmount={pot.goalAmount}
+                    percentage={pot.percentageComplete}
+                    filled
+                  />
+                  <SubSection noPadding noAnchor>
+                    Last deposit:{' '}
+                    {pot.transactionalData &&
+                    pot.transactionalData.lastDepositAmount
+                      ? `£${pot.transactionalData.lastDepositAmount / 100}`
+                      : 'loading...'}
+                  </SubSection>
+                  <SubSection noPadding noAnchor>
+                    Last withdrawal:{' '}
+                    {pot.transactionalData &&
+                    pot.transactionalData.lastWithdrawalAmount
+                      ? `£${pot.transactionalData.lastWithdrawalAmount / 100}`
+                      : 'loading...'}
+                  </SubSection>
+                  <br />
+                  <br />
+                </Fragment>
               ))}
           </Section>
         </Section>
