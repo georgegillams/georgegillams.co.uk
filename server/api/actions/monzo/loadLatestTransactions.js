37b61f1b5b3a29c47c036cff90d8fea37408b1aa
George Gillams <george.gillams@skyscanner.net>
2019-07-30 08:42:42 +0100

    Monzo refactor

--- server/api/actions/monzo/loadLatestTransactions.js
+++ server/api/actions/monzo/loadLatestTransactions.js
@@ -2,120 +2,94 @@ import { URLSearchParams } from 'url';
 
 import fetch from 'node-fetch';
 
-import { datumLoadSingle } from '../../actions/datum';
-
+import { authMonzo, loadPotData } from './helpers';
 import POT_CONFIGS from './potConfigs';
 
 function loadLatestTransactions(req) {
   return new Promise(resolve => {
-    datumLoadSingle({
-      redisKey: 'monzoApiKeys',
-      resolveIfNotFound: true,
-      sortKey: 'lastUpdatedTimestamp',
-    }).then(accessToken => {
-      const accessPassword = process.env.MONZO_ACCESS_PASSWORD;
-
-      if (!accessToken) {
-        resolve({ error: 'No access token configured' });
+    authMonzo(req.body.password).then(authResult => {
+      if (authResult.error || authResult.warning) {
+        resolve(authResult);
         return;
       }
+      loadPotData(req.body.password).then(potData => {
+        if (potData.error || potData.warning) {
+          resolve(potData);
+          return;
+        }
 
-      if (!req.body.password || req.body.password !== accessPassword) {
-        resolve({ warning: 'Access password incorrect' });
-        return;
-      }
+        fetch('https://api.monzo.com/accounts', {
+          method: 'get',
+          headers: {
+            Authorization: `Bearer ${authResult.accessToken.key}`,
+          },
+        })
+          .then(res => res.json())
+          .then(accountData => {
+            const accountID = accountData.accounts.find(
+              a => a.type === 'uk_retail',
+            ).id;
 
-      fetch('https://api.monzo.com/accounts', {
-        method: 'get',
-        headers: {
-          Authorization: `Bearer ${accessToken.key}`,
-        },
-      })
-        .then(res => res.json())
-        .then(accountData => {
-          const accountID = accountData.accounts.find(
-            a => a.type === 'uk_retail',
-          ).id;
+            const params = new URLSearchParams();
+            params.append('account_id', accountID);
 
-          const params = new URLSearchParams();
-          params.append('account_id', accountID);
-
-          fetch(`https://api.monzo.com/pots`, {
-            method: 'GET',
-            headers: {
-              Authorization: `Bearer ${accessToken.key}`,
-            },
-          })
-            .then(res => res.json())
-            .then(potData => {
-              if (!potData || !potData.pots) {
-                resolve({
-                  error:
-                    'The Monzo token has expired. Tell George to generate a new one.',
-                });
-                return;
+            const potConfigsAnnotated = POT_CONFIGS.map(pc => {
+              const pot = potData.find(p => p.name === pc.name && !p.deleted);
+              if (!pot) {
+                return pc;
               }
-              const potConfigsAnnotated = POT_CONFIGS.map(pc => {
-                const pot = potData.pots.find(
-                  p => p.name === pc.name && !p.deleted,
-                );
-                if (!pot) {
-                  return pc;
-                }
-                const potId = pot.id;
-                return {
-                  ...pc,
-                  potId,
-                };
-              });
-              fetch(`https://api.monzo.com/transactions?${params.toString()}`, {
-                method: 'GET',
-                headers: {
-                  Authorization: `Bearer ${accessToken.key}`,
-                },
-              })
-                .then(res => res.json())
-                .then(transactionData => {
-                  let potTransfers = transactionData.transactions.filter(t => {
-                    let potOfInterest = false;
-                    potConfigsAnnotated.forEach(pc => {
-                      if (pc.potId === t.metadata.pot_id) {
-                        potOfInterest = true;
-                      }
-                    });
-                    return t.scheme === 'uk_retail_pot' && potOfInterest;
-                  });
-                  potTransfers = potTransfers.reverse().map(p => {
-                    const potName = potConfigsAnnotated.find(
-                      pc => pc.potId === p.metadata.pot_id,
-                    ).name;
-                    return { amount: p.amount, potName };
-                  });
-                  const potWithdrawals = potTransfers.filter(
-                    pt => pt.amount > 0,
-                  );
-                  const potDeposits = potTransfers.filter(pt => pt.amount < 0);
-                  const processedData = POT_CONFIGS.map(pc => {
-                    const matchingWithdrawal = potWithdrawals.find(
-                      pw => pw.potName === pc.name,
-                    );
-                    const matchingDeposit = potDeposits.find(
-                      pw => pw.potName === pc.name,
-                    );
-                    return {
-                      name: pc.name,
-                      lastDepositAmount: matchingDeposit
-                        ? -matchingDeposit.amount
-                        : 0,
-                      lastWithdrawalAmount: matchingWithdrawal
-                        ? matchingWithdrawal.amount
-                        : 0,
-                    };
-                  });
-                  resolve(processedData);
-                });
+              const potId = pot.id;
+              return {
+                ...pc,
+                potId,
+              };
             });
-        });
+            fetch(`https://api.monzo.com/transactions?${params.toString()}`, {
+              method: 'GET',
+              headers: {
+                Authorization: `Bearer ${authResult.accessToken.key}`,
+              },
+            })
+              .then(res => res.json())
+              .then(transactionData => {
+                let potTransfers = transactionData.transactions.filter(t => {
+                  let potOfInterest = false;
+                  potConfigsAnnotated.forEach(pc => {
+                    if (pc.potId === t.metadata.pot_id) {
+                      potOfInterest = true;
+                    }
+                  });
+                  return t.scheme === 'uk_retail_pot' && potOfInterest;
+                });
+                potTransfers = potTransfers.reverse().map(p => {
+                  const potName = potConfigsAnnotated.find(
+                    pc => pc.potId === p.metadata.pot_id,
+                  ).name;
+                  return { amount: p.amount, potName };
+                });
+                const potWithdrawals = potTransfers.filter(pt => pt.amount > 0);
+                const potDeposits = potTransfers.filter(pt => pt.amount < 0);
+                const processedData = POT_CONFIGS.map(pc => {
+                  const matchingWithdrawal = potWithdrawals.find(
+                    pw => pw.potName === pc.name,
+                  );
+                  const matchingDeposit = potDeposits.find(
+                    pw => pw.potName === pc.name,
+                  );
+                  return {
+                    name: pc.name,
+                    lastDepositAmount: matchingDeposit
+                      ? -matchingDeposit.amount
+                      : 0,
+                    lastWithdrawalAmount: matchingWithdrawal
+                      ? matchingWithdrawal.amount
+                      : 0,
+                  };
+                });
+                resolve(processedData);
+              });
+          });
+      });
     });
   });
 }
--- server/api/actions/monzo/loadPots.js
+++ server/api/actions/monzo/loadPots.js
@@ -1,89 +1,40 @@
-import fetch from 'node-fetch';
-import moment from 'moment';
-
-import { datumLoadSingle } from '../../actions/datum';
-
+import { loadPotData, getMonthsElapsedPercentage } from './helpers';
 import POT_CONFIGS from './potConfigs';
-
-function getMonthsElapsedPercentage(potName) {
-  if (
-    potName.includes('(monthly)') ||
-    potName === 'Weekly' ||
-    potName === 'Leftover'
-  ) {
-    return 0;
-  }
-
-  const config = POT_CONFIGS.filter(p => p.name === potName)[0];
-
-  const result = moment().diff(config.startDate, 'months');
-  return Math.min(100, (result * 100) / 12);
-}
 
 function loadPots(req) {
   return new Promise(resolve => {
-    datumLoadSingle({
-      redisKey: 'monzoApiKeys',
-      resolveIfNotFound: true,
-      sortKey: 'lastUpdatedTimestamp',
-    }).then(accessToken => {
-      const accessPassword = process.env.MONZO_ACCESS_PASSWORD;
-
-      if (!accessToken) {
-        resolve({ error: 'No access token configured' });
+    loadPotData(req.body.password).then(potData => {
+      if (potData.error || potData.warning) {
+        resolve(potData);
         return;
       }
+      const processedData = POT_CONFIGS.map(potConfig => {
+        const pot = potData.filter(
+          p => p.name === potConfig.name && !p.deleted,
+        )[0];
+        if (!pot) {
+          resolve({ error: 'An unknown error occured' });
+          return null;
+        }
 
-      if (!req.body.password || req.body.password !== accessPassword) {
-        resolve({ warning: 'Access password incorrect' });
-        return;
-      }
-
-      fetch('https://api.monzo.com/pots', {
-        method: 'get',
-        headers: {
-          Authorization: `Bearer ${accessToken.key}`,
-        },
-      })
-        .then(res => res.json())
-        .then(data => {
-          if (!data || !data.pots) {
-            resolve({
-              error:
-                'The Monzo token has expired. Tell George to generate a new one.',
-            });
-            return;
-          }
-
-          const processedData = POT_CONFIGS.map(potConfig => {
-            const pot = data.pots.filter(
-              p => p.name === potConfig.name && !p.deleted,
-            )[0];
-            if (!pot) {
-              return null;
-            }
-
-            const goalAmount = parseFloat(pot.goal_amount) / 100;
-            const balance = parseFloat(pot.balance) / 100;
-            const monthsElapsedPercentage = getMonthsElapsedPercentage(
-              pot.name,
-            );
-            const expectedSavingsSoFar =
-              (goalAmount * monthsElapsedPercentage) / 100;
-            const shortfall = expectedSavingsSoFar - balance;
-            return {
-              name: pot.name,
-              balance,
-              goalAmount: parseFloat(pot.goal_amount) / 100,
-              percentageTimeElapsed: monthsElapsedPercentage,
-              shortfall: shortfall < 5 ? null : shortfall,
-              percentageComplete: pot.goal_amount
-                ? Math.ceil((100 * pot.balance) / pot.goal_amount)
-                : 100,
-            };
-          });
-          resolve(processedData);
-        });
+        const goalAmount = parseFloat(pot.goal_amount) / 100;
+        const balance = parseFloat(pot.balance) / 100;
+        const monthsElapsedPercentage = getMonthsElapsedPercentage(pot.name);
+        const expectedSavingsSoFar =
+          (goalAmount * monthsElapsedPercentage) / 100;
+        const shortfall = expectedSavingsSoFar - balance;
+        return {
+          name: pot.name,
+          balance,
+          goalAmount: parseFloat(pot.goal_amount) / 100,
+          percentageTimeElapsed: monthsElapsedPercentage,
+          shortfall: shortfall < 5 ? null : shortfall,
+          percentageComplete: pot.goal_amount
+            ? Math.ceil((100 * pot.balance) / pot.goal_amount)
+            : 100,
+        };
+      });
+      resolve(processedData);
     });
   });
 }
