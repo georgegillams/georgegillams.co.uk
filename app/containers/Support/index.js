9b7925bb238f799e5025bb3dd82738396e86b601
George Gillams <george.gillams@skyscanner.net>
2019-10-16 08:06:49 +0100

    [NO-JIRA] add redux helper for composing container

--- app/containers/Support/index.js
+++ app/containers/Support/index.js
@@ -1,34 +1,16 @@
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { createStructuredSelector } from 'reselect';
-import actionMeta from './actionMeta';
-
-import { selectors, actions, reducer } from './redux-definitions';
-import { mapSelectors, mapActions } from 'helpers/redux';
 import appSelectors from 'containers/App/selectors';
+
+import { composeContainer } from 'helpers/redux';
+import actionMeta from './actionMeta';
+import { selectors, actions, reducer } from './redux-definitions';
 import saga from './saga';
 import Support from './Support';
 
-import injectSaga from 'utils/injectSaga';
-import injectReducer from 'utils/injectReducer';
-
-const mapDispatchToProps = dispatch => mapActions(dispatch, { ...actions });
-
-const mapStateToProps = createStructuredSelector(
-  mapSelectors({ ...selectors, ...appSelectors }),
+module.exports = composeContainer(
+  Support,
+  actionMeta.key,
+  { ...selectors, ...appSelectors },
+  { ...actions },
+  reducer,
+  saga,
 );
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-);
-
-const withReducer = injectReducer({ key: actionMeta.key, reducer });
-const withSaga = injectSaga({ key: actionMeta.key, saga });
-
-export default compose(
-  withReducer,
-  withSaga,
-  withConnect,
-)(Support);
-export { mapDispatchToProps };
