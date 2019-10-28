8d4c58b58e337161dd16a1afb4bdf00f8d7a745e
George Gillams <george.gillams@skyscanner.net>
2019-10-28 21:23:41 +0000

    [NO-JIRA] Redesign work page (#290)

--- app/containers/Work/Work.js
+++ app/containers/Work/Work.js
@@ -25,92 +25,136 @@ export default class Work extends Component {
     return (
       <div className={getClassName('pages__container--prose')}>
         <Helmet title="Work" />
-        <Section name="My Work">
+        <Section name="Work">
+          <SubSection name="Fundamentally open-source">
+            I believe that when we share, we all get more out of the code we
+            write. That’s why I always prefer working on open-source projects.
+          </SubSection>
+
+          <SubSection name="Browser scripts">
+            I automate pretty much everything I can, often in the form of
+            browser-scripts. I have built several little nuggets of JS that help
+            me on a day to day basis, and they can be run in any browser via an
+            extension such as{' '}
+            <TextLink external href="https://www.tampermonkey.net/">
+              Tamper Monkey
+            </TextLink>
+            .
+            <br />
+            <Button
+              className={getClassName('pages__component--upper-margin')}
+              hrefExternal
+              href="https://github.com/georgegillams/browser-scripts/"
+            >
+              Browse and install my browser-scripts →
+            </Button>
+          </SubSection>
+
+          <SubSection name="Redux definitions">
+            After a while using Redux, I became frustrated with the
+            overly-verbose and repetitive nature of its components. I,
+            therefore, wrote some helpers to reduce the repetitiveness, and
+            eventually, they grew into a complete library for generating redux
+            components from a single source of truth. Because of the organic way
+            in which I built this from the ground up, the library is completely
+            interoperable with hand-coded redux components, meaning that it can
+            be adopted gradually in an existing codebase using redux.
+            <br />
+            <Button
+              className={getClassName('pages__component--upper-margin')}
+              hrefExternal
+              href="https://github.com/georgegillams/redux-definitions/"
+            >
+              Check out redux-definitions on GitHub →
+            </Button>
+          </SubSection>
+
+          <SubSection name="Party Parrot">
+            As a developer and advocate for open source, having a contribution
+            to the{' '}
+            <TextLink external href="https://cultofthepartyparrot.com/">
+              official Party Parrot repo{' '}
+            </TextLink>{' '}
+            was a proud moment.
+            <br />
+            <img
+              width={60}
+              src="https://cultofthepartyparrot.com/parrots/hd/opensourceparrot.gif"
+            />
+          </SubSection>
+
+          <SubSection name="EPICC Conference">
+            For a non-profit conference running in Southampton, I developed a
+            website where delegates could reserve and pay for tickets. The
+            website also allowed us to collect information needed from
+            delegates, and a QR-code based ticket system was implemented to
+            quickly register delegates when arriving at the conference.
+            <br />
+            <br />
+            The stack was as follows: React front-end, Node backend, Redis DB,
+            Stripe payment handling, Travis CI and Heroku deployment.
+            <br />
+            <br />
+            The custom-built solution allowed us to sell nearly 120 tickets with
+            lower fees than other event/ticket selling products, allowed
+            delegates to manage their own tickets and enabled the conference
+            committee to instantly admit users with valid tickets to the
+            conference using a browser-based QR code scanner.
+          </SubSection>
+
+          <SubSection name="Backpack">
+            <TextLink external href="https://backpack.github.io/">
+              Backpack
+            </TextLink>{' '}
+            is Skyscanner's open-source design system which supports 4 platforms
+            (Android, iOS, React Native and Web). Our work which combines Design
+            and Engineering in equal measures enables fast development and
+            reduces effort duplication.
+            <br />
+            <br />A recent highlight has been delivering Skyscanner’s new brand
+            across all our products, leveraging the coverage of Backpack
+            libraries to enable seamless experimentation and roll-out across the
+            entire ecosystem. And now we’re applying our learnings to rollout
+            Dark Mode support too. 🎉
+            <br />
+            <Button
+              className={getClassName('pages__component--upper-margin')}
+              hrefExternal
+              href="https://backpack.github.io/"
+            >
+              Read more about Backpack here →
+            </Button>
+          </SubSection>
+
           <SubSection name="Software Engineering Masters">
             I have a 1st class Masters in Software Engineering from the
             University of Southampton.
             <br />
-            <br />
-            <Button href="/work/degree">Information about my degree →</Button>
+            <Button
+              className={getClassName('pages__component--upper-margin')}
+              href="/work/degree"
+            >
+              Information about my degree →
+            </Button>
           </SubSection>
-          <SubSection name="Work">
-            I started my Software Engineering career at{' '}
+
+          <SubSection name="EWOS">
+            In 2015, I joined the EWOS (Electronic Warfare Operational Support)
+            team at{' '}
             <TextLink
               href="http://www.leonardocompany.com/en/-/ewos-electronic_warfare_operational_support"
               external
             >
-              Leonardo{' '}
+              Leonardo
             </TextLink>
-            working on .NET frameworks for analysis tools.
-            <br />I now work at Skyscanner, on their open-source design system,{' '}
-            <TextLink href="https://backpack.github.io/" external>
-              Backpack{' '}
-            </TextLink>
+            . My work there involved maintaining large .NET frameworks which
+            supported feature-rich mission analysis tools. Close collaboration
+            with the hardware team and our consumers was essential and demanded
+            a strong, agile approach to development. Thorough planning, in which
+            we were all involved, was key to our success. Throughout some 60
+            weeks there, I immensely improved my ability to navigate large
+            codebases and to produce readable, manageable code myself.
           </SubSection>
-          <Section name="Portfolio">
-            <SubSection name="Party Parrot">
-              As a developer and advocate for open source, having a contribution
-              to the{' '}
-              <TextLink external href="https://cultofthepartyparrot.com/">
-                official Party Parrot repo{' '}
-              </TextLink>{' '}
-              was a proud moment.
-              <br />
-              <img
-                width={60}
-                src="https://cultofthepartyparrot.com/parrots/hd/opensourceparrot.gif"
-              />
-            </SubSection>
-            <SubSection name="EPICC Conference">
-              For a non-profit conference running in Southampton, I developed a
-              website where delegates could reserve and pay for tickets. The
-              website also allowed us to collect information needed from
-              delegates, and a QR-code based ticket system was implemented to
-              quickly register delegates when arriving at the conference.
-              <br />
-              <br />
-              The stack was as follows: React front-end, Node backend, Redis DB,
-              Stripe payment handling, Travis CI and Heroku deployment.
-              <br />
-              <br />
-              The custom built solution allowed us to sell nearly 120 tickets,
-              allowing delegates to manage their own tickets and allowing the
-              conference committee to instantly admit users with valid tickets
-              to the conference.
-            </SubSection>
-            <SubSection name="Backpack">
-              <TextLink external href="https://backpack.github.io/">
-                Backpack
-              </TextLink>{' '}
-              is Skyscanner's open-source design system which supports 4
-              platforms (Android, iOS, React Native and Web). Our work which
-              combines Design and Engineering in equal measures enables fast
-              development and reduces effort duplication.
-              <br />
-              <br />
-              <Button hrefExternal href="https://backpack.github.io/">
-                Read more about Backpack here →
-              </Button>
-            </SubSection>
-            <SubSection name="EWOS">
-              In 2015, I joined the EWOS (Electronic Warfare Operational
-              Support) team at{' '}
-              <TextLink
-                href="http://www.leonardocompany.com/en/-/ewos-electronic_warfare_operational_support"
-                external
-              >
-                Leonardo{' '}
-              </TextLink>
-              . My work there involved maintaining large .NET frameworks which
-              supported feature-rich mission analysis tools. Close collaboration
-              with the hardware team and our consumers was essential and
-              demanded a strong, agile approach to development. Thorough
-              planning, in which we were all involved, was key to our success.
-              Throughout some 60 weeks there, I immensely improved my ability to
-              navigate large codebases and to produce readable, manageable code
-              myself.
-            </SubSection>
-          </Section>
         </Section>
       </div>
     );
