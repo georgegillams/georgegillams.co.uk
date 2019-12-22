4febe3141ace1c52201759c26e7ea2c2edf00598
George Gillams <george.gillams@skyscanner.net>
2019-12-22 23:01:41 +0000

    [NO-JIRA] Update monzo pot configs

--- server/api/actions/monzo/potConfigs.js
+++ server/api/actions/monzo/potConfigs.js
@@ -11,12 +11,11 @@ const POT_CONFIGS = [
   {
     name: 'Holiday',
     startDate: `${thisYear}-12-01`,
-    startAmount: 291.67,
   },
   {
     name: 'Emergencies',
     startDate: `${thisYear}-12-01`,
-    startAmount: 291.67,
+    startAmount: 100,
   },
   {
     name: 'Gifts',
@@ -25,23 +24,27 @@ const POT_CONFIGS = [
   {
     name: 'Charlie',
     startDate: `${thisYear}-12-01`,
+    startAmount: 291,
   },
   {
     name: 'Rents',
     startDate: `${thisYear}-12-01`,
+    startAmount: 170,
   },
   {
     name: 'Extras',
     startDate: `${thisYear}-12-01`,
-    startAmount: 291.67,
+    startAmount: 100,
   },
   {
-    name: 'Subscriptions (yearly) - Dropbox, Todoist and Domains',
+    name: 'Yearly Subscriptions',
     startDate: `${thisYear}-12-01`,
+    startAmount: 60,
   },
   {
     name: 'Dentist and prescriptions',
     startDate: `${thisYear}-12-01`,
+    startAmount: 30,
   },
   {
     name: 'Buffer',
