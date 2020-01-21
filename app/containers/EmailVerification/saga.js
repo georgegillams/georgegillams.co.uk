9b76754a78057d49ce57056b56df4411a298a981
George Gillams <george.gillams@skyscanner.net>
2020-01-21 18:37:07 +0000

    [NO-JIRA] Email verification with sagaHelper

--- app/containers/EmailVerification/saga.js
+++ app/containers/EmailVerification/saga.js
@@ -8,6 +8,7 @@ import { call, put, select, takeLatest } from 'redux-saga/effects';
 import { setUser } from 'containers/App/actions';
 import { API_ENDPOINT, COMMUNICATION_ERROR_MESSAGE } from 'helpers/constants';
 import { pushMessage } from 'containers/RequestStatusWrapper/actions';
+import { sagaHelper } from 'helpers/redux/saga';
 import request from 'utils/request';
 
 const emailVerifiedMessage = { type: 'success', message: 'Email verified!' };
@@ -16,27 +17,22 @@ export function* doVerification() {
   const token = yield select(makeSelectToken());
   const requestURL = `${API_ENDPOINT}/verifyemail`;
 
-  try {
-    const verificationResult = yield call(request, requestURL, {
-      method: 'POST',
-      body: JSON.stringify({ verificationKey: token }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }); // Can add third arg for options
-    if (verificationResult.error) {
-      yield put(verifyEmailRegisterError(verificationResult));
-      yield put(
-        pushMessage({ type: 'error', message: verificationResult.errorMessage }),
-      );
-    } else {
-      yield put(verifyEmailRegisterSuccess());
-      yield put(pushMessage(emailVerifiedMessage));
-    }
-  } catch (err) {
-    yield put(verifyEmailRegisterError(err));
-    yield put(pushMessage(COMMUNICATION_ERROR_MESSAGE));
-  }
+  const requestParams = {
+    method: 'POST',
+    body: JSON.stringify({ verificationKey: token }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  yield sagaHelper(
+    requestURL,
+    requestParams,
+    verifyEmailRegisterError,
+    verifyEmailRegisterSuccess,
+    emailVerifiedMessage,
+    null,
+  );
 }
 
 export default function* verify() {
