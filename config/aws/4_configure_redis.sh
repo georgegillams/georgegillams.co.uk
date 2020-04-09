ec3aae10404a1a3a5bb6dd2f79cd35bd6c8c62b9
George Gillams <george.gillams@skyscanner.net>
2020-04-09 01:23:11 +0100

    [NO-JIRA] Add more scripts

---
+++ config/aws/4_configure_redis.sh
@@ -0,0 +1,3 @@
+sudo cp ./config/aws/redis.conf /etc/redis/redis.conf
+sudo chown ubuntu /etc/redis/redis_log
+sudo chown ubuntu /etc/redis/redis.conf
--- config/aws/setup.sh
+++ config/aws/setup.sh
@@ -1,3 +1,4 @@
 . ./config/aws/1_install_nvm.sh
 . ./config/aws/2_setup_pm2.sh
 . ./config/aws/3_configure_nginx.sh
+. ./config/aws/4_configure_redis.sh
