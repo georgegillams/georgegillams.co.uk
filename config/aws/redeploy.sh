a10f467cc55fab3de13feb32824b926cc3df792d
George Gillams <george.gillams@skyscanner.net>
2020-04-21 16:39:22 +0100

    [NO-JIRA] Prevent duplicate redeploy ops

--- config/aws/redeploy.sh
+++ config/aws/redeploy.sh
@@ -2,12 +2,13 @@
 
 cd /home/ubuntu
 if [ -d build ]; then
-  sleep 10 # wait to ensure the file transfer is complete
+  sleep 30 # wait to ensure the file transfer is complete
+  mv build buildInProgress
   cd georgegillams.co.uk
   git fetch && git reset --hard origin/master && git pull
   PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm ci
   pm2 stop all
-  rm -rf build && mv ../build ./
+  rm -rf build && mv ../buildInProgress ./build
   pm2 start all
 else
   echo "No new version to deploy"
