8ea311181413623f991f3ba0d48e2555801ddf30
George Gillams <george.gillams@skyscanner.net>
2020-05-03 15:53:25 +0100

    [NO-JIRA] PM2 tweaks

--- server/api/actions/actionTest.js
+++ server/api/actions/actionTest.js
@@ -1,5 +1,6 @@
 export default function load(req) {
   return new Promise((resolve, reject) => {
-    resolve({ testValue: '15' });
+    resolve({ testValue: '30' });
+>>>>>>> ed34b11... [NO-JIRA] PM2 tweaks
   });
 }
