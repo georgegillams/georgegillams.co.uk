140236a34993bb7227c4317a1a3bfc658292fc7b
George Gillams <george.gillams@skyscanner.net>
2020-05-29 21:10:22 +0100

    [NO-JIRA] API Code review 7

--- server/api/actions/auth/load.js
+++ server/api/actions/auth/load.js
@@ -1,22 +1,19 @@
 import authentication from 'utils/authentication';
 
 export default function loadAuth(req) {
-  return new Promise(resolve => {
-    authentication(req).then(user => {
-      if (user) {
-        resolve({
-          user: {
-            id: user.id,
-            name: user.name,
-            uname: user.uname,
-            email: user.email,
-            emailVerified: user.emailVerified,
-            admin: user.admin,
-          },
-        });
-      } else {
-        resolve({ user: null });
-      }
-    });
+  return authentication(req).then(user => {
+    if (user) {
+      return {
+        user: {
+          id: user.id,
+          name: user.name,
+          uname: user.uname,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          admin: user.admin,
+        },
+      };
+    }
+    return { user: null };
   });
 }
