835dafbbfeb9716695b055e13080036c21444603
George Gillams <george.gillams@skyscanner.net>
2021-01-01 15:53:30 +0000

    [NO-JIRA] New homepage photo section

--- src/components/HomePageV2/PhotoSection/PhotoSection.js
+++ src/components/HomePageV2/PhotoSection/PhotoSection.js
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import { Image } from 'gg-components/Image';
 import STYLES from './photo-section.scss';
+import AnimatedImage from './AnimatedImage';
+import { withScroll } from 'gg-components/ScrollContainer';
 import { cssModules } from 'gg-components/helpers/cssModules';
 
 const getClassName = cssModules(STYLES);
 
 const LifeSection = props => {
-  const { className, ...rest } = props;
+  const { hasBeenMostlyInView, hasBeenFullyInView, className, ...rest } = props;
+
+  const [entryDelayDone, setEntryDelayDone] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setEntryDelayDone(true);
+    }, 2000);
+  }, []);
+
+  delete rest.fullyInView;
+  delete rest.hasBeenInView;
+  delete rest.hasBeenJustInView;
+  delete rest.hasBeenOutOfView;
+  delete rest.inView;
+  delete rest.justInView;
+  delete rest.mostlyInView;
+  delete rest.outOfView;
+  delete rest.scrollPosition;
+  delete rest.scrollPositionVh;
 
   return (
-    <div className={getClassName(className)} style={{ width: '70%' }} {...rest}>
-      <Image
-        className={getClassName('about__image')}
-        imgProps={{
-          alt: 'Me',
-        }}
-        aspectX={3829}
-        aspectY={2872}
-        lightSrc="https://i.imgur.com/FLA0jkg.jpg"
-        darkSrc="https://i.imgur.com/FLA0jkg.jpg"
-      />
+    <div className={getClassName('photo-section__outer', className)} {...rest}>
+      <div className={getClassName('photo-section__content')} {...rest}>
+        <AnimatedImage show={entryDelayDone && (hasBeenMostlyInView || hasBeenFullyInView)} />
+      </div>
     </div>
   );
 };
 
-LifeSection.propTypes = { className: PropTypes.string };
+LifeSection.propTypes = {
+  hasBeenFullyInView: PropTypes.bool.isRequired,
+  hasBeenMostlyInView: PropTypes.bool.isRequired,
+  className: PropTypes.string,
+};
 
 LifeSection.defaultProps = { className: null };
 
-export default LifeSection;
+export default withScroll(LifeSection);
