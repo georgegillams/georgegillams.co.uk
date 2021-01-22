4cb4415de707e5faa8e4be75fbf5532f45166fb8
George Gillams <george.gillams@skyscanner.net>
2021-01-22 20:02:02 +0000

    [NO-JIRA] Fix work section force-layout

--- src/components/HomePageV2/WorkSection/SlidingPhone.js
+++ src/components/HomePageV2/WorkSection/SlidingPhone.js
@@ -4,28 +4,34 @@ import PropTypes from 'prop-types';
 import { Image } from 'gg-components/Image';
 import STYLES from './sliding-phone.scss';
 import { cssModules } from 'gg-components/helpers/cssModules';
-import { useEntryAnimationClientOnly } from 'gg-components/ServerSideRendering';
 
 const getClassName = cssModules(STYLES);
 
 const SlidingPhone = props => {
   const { show, className, lightSrc, darkSrc, ...rest } = props;
 
-  const [isFirstRender, animationsEnabled] = useEntryAnimationClientOnly();
-
   return (
     <div className={getClassName('sliding-phone__outer', className)} {...rest}>
       <Image
         className={getClassName(
           'sliding-phone__image',
-          !isFirstRender && !show && 'sliding-phone__image--hide',
-          animationsEnabled && 'sliding-phone__image--animated'
+          !show && 'sliding-phone__image--hide',
+          'sliding-phone__image--animated'
         )}
         aspectX={1004}
         aspectY={1986}
         lightSrc={lightSrc}
         darkSrc={darkSrc}
       />
+      <noscript>
+        <Image
+          className={getClassName('sliding-phone__image')}
+          aspectX={1004}
+          aspectY={1986}
+          lightSrc={lightSrc}
+          darkSrc={darkSrc}
+        />
+      </noscript>
     </div>
   );
 };
