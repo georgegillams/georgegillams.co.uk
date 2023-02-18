a9bd828026d1b7b4b7613e4350775a831a477b9e
George Gillams <code@georgegillams.co.uk>
2023-02-18 18:02:10 +0000

    feat: Add photo cache

--- server/api/actions/photo-showcase/loadAll.js
+++ server/api/actions/photo-showcase/loadAll.js
@@ -3,6 +3,10 @@ import MockFlickr from './MockFlickr';
 
 const flickr = process.env.FLICKR_API_KEY ? new Flickr(process.env.FLICKR_API_KEY) : new MockFlickr();
 
+const cache = {};
+
+const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
+
 const getPhotoSize = async photoId => {
   const res = await flickr.photos.getSizes({ photo_id: photoId });
   const sizes = res.body.sizes.size;
@@ -29,16 +33,32 @@ const attachPhotoInformation = async photo => {
   return { ...photo, size: photoSize, information: photoInformation };
 };
 
-export default async function loadAll() {
+const updateCachedValue = async () => {
+  cache.expiry = Date.now() + ONE_DAY_IN_MS;
+
   // https://www.flickr.com/services/api/flickr.photosets.getPhotos.html
   const res = await flickr.photosets.getPhotos({ photoset_id: '72177720301327018', user_id: '137198167@N03' });
-  const photos = res.body.photoset.photo;
+  const photos = res.body.photoset.photo.reverse();
   const photosWithInformation = await Promise.all(photos.map(photo => attachPhotoInformation(photo)));
-  return {
+  const apiResult = {
     photos: photosWithInformation.map(photo => ({
       key: photo.id,
       ...photo.size,
       alt: photo.information.description,
     })),
   };
+
+  cache.value = apiResult;
+};
+
+export default async function loadAll() {
+  if (!cache.value) {
+    // If no cached value, we have to wait for it to be fetched and then return it
+    await updateCachedValue();
+  } else if (cache.expiry < Date.now()) {
+    // If the cached value is expired, we want to update it. But we don't need to wait for it to be updated before returning the currently cached version
+    updateCachedValue();
+  }
+
+  return cache.value;
 }
