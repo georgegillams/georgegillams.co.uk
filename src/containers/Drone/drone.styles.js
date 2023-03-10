070c18a22276fdcad97c2ef685514c5ca862eebc
George Gillams <code@georgegillams.co.uk>
2023-03-10 06:14:18 +0000

    feat: Update drone page (#1024)

--- src/containers/Drone/drone.styles.js
+++ src/containers/Drone/drone.styles.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 import Image from '@george-gillams/components/image';
-import { spacingLg } from '@george-gillams/components/constants/layout';
+import { breakpointMd, spacingLg } from '@george-gillams/components/constants/layout';
 
 export const StyledImage = styled(Image)`
-  width: 80%;
+  width: 100%;
   margin-top: ${spacingLg};
+
+  @media (min-width: ${breakpointMd}) {
+    width: 80%;
+  }
 `;
