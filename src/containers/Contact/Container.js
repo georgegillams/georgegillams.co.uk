02cbf74c08a35e593ea90381b8c01cbc22ea067e
George Gillams <code@georgegillams.co.uk>
2023-05-21 18:08:39 +0100

    fix: Fix contact info-cell widths (#1039)

--- src/containers/Contact/Container.js
+++ src/containers/Contact/Container.js
@@ -13,121 +13,123 @@ import emailLogo from './email.svg';
 import unsplashLogo from './unsplash.svg';
 import formLogo from './form.svg';
 import PageContainer, { WIDTHS } from 'components/common/PageContainer';
-import { IconImage, ImageIconContainer, Spacer, StyledInfoCellWithScroll } from './contact.styles';
+import { IconImage, ImageIconContainer, InfoCellContainer, Spacer, StyledInfoCellWithScroll } from './contact.styles';
 
 const Contact = () => {
   return (
     <PageContainer width={WIDTHS.fullWidth} centred>
       <PageTitle name="Get in touch" style={{ width: '100%' }}>
         <Spacer />
-        <StyledInfoCellWithScroll
-          title="Form"
-          content={
-            <TextLink href="https://form.typeform.com/to/WJSWmnKh" hrefExternal>
-              Fill in my contact form
-            </TextLink>
-          }
-          aux={
-            <ImageIconContainer>
-              <IconImage invertInDarkMode src={formLogo.src} />
-            </ImageIconContainer>
-          }
-        />
-        <StyledInfoCellWithScroll
-          cellStyle={INFO_CELL_STYLES.dark}
-          title="Twitter"
-          content={
-            <TextLink href="https://twitter.com/georgegillams" hrefExternal>
-              See my profile on Twitter
-            </TextLink>
-          }
-          aux={
-            <ImageIconContainer>
-              <IconImage src={twitterLogo.src} />
-            </ImageIconContainer>
-          }
-        />
-        <StyledInfoCellWithScroll
-          title="Linkedin"
-          content={
-            <TextLink href="https://www.linkedin.com/in/george-gillams-37537077" hrefExternal>
-              See my profile on Linkedin
-            </TextLink>
-          }
-          aux={
-            <ImageIconContainer>
-              <IconImage src={linkedInLogo.src} />
-            </ImageIconContainer>
-          }
-        />
-        <StyledInfoCellWithScroll
-          cellStyle={INFO_CELL_STYLES.dark}
-          title="GitHub"
-          content={
-            <TextLink href="https://github.com/georgegillams" hrefExternal>
-              See my profile on GitHub
-            </TextLink>
-          }
-          aux={
-            <ImageIconContainer>
-              <IconImage invertInDarkMode src={githubLogo.src} />
-            </ImageIconContainer>
-          }
-        />
-        <StyledInfoCellWithScroll
-          title="Email"
-          content={
-            <TextLink href="mailto:hello@georgegillams.co.uk" hrefExternal>
-              Email hello@georgegillams.co.uk
-            </TextLink>
-          }
-          aux={
-            <ImageIconContainer>
-              <IconImage invertInDarkMode src={emailLogo.src} />
-            </ImageIconContainer>
-          }
-        />
-        <StyledInfoCellWithScroll
-          cellStyle={INFO_CELL_STYLES.dark}
-          title="Flickr"
-          content={
-            <TextLink href="https://www.flickr.com/photos/georgegillams" hrefExternal>
-              See my profile on Flickr
-            </TextLink>
-          }
-          aux={
-            <ImageIconContainer>
-              <IconImage src={flickrLogo.src} />
-            </ImageIconContainer>
-          }
-        />
-        <StyledInfoCellWithScroll
-          title="Gurushots"
-          content={
-            <TextLink href="https://gurushots.com/georgegillams/photos" hrefExternal>
-              See my profile on Gurushots
-            </TextLink>
-          }
-          aux={
-            <ImageIconContainer>
-              <IconImage invertInDarkMode src={gurushotsLogo.src} />
-            </ImageIconContainer>
-          }
-        />
-        <StyledInfoCellWithScroll
-          cellStyle={INFO_CELL_STYLES.dark}
-          title="Unsplash"
-          content={
-            <TextLink href="https://unsplash.com/@georgegillams/" hrefExternal>
-              See my profile on Unsplash
-            </TextLink>
-          }
-          aux={
-            <ImageIconContainer>
-              <IconImage invertInDarkMode src={unsplashLogo.src} />
-            </ImageIconContainer>
-          }
-        />
+        <InfoCellContainer>
+          <StyledInfoCellWithScroll
+            title="Form"
+            content={
+              <TextLink href="https://form.typeform.com/to/WJSWmnKh" hrefExternal>
+                Fill in my contact form
+              </TextLink>
+            }
+            aux={
+              <ImageIconContainer>
+                <IconImage invertInDarkMode src={formLogo.src} />
+              </ImageIconContainer>
+            }
+          />
+          <StyledInfoCellWithScroll
+            cellStyle={INFO_CELL_STYLES.dark}
+            title="Twitter"
+            content={
+              <TextLink href="https://twitter.com/georgegillams" hrefExternal>
+                See my profile on Twitter
+              </TextLink>
+            }
+            aux={
+              <ImageIconContainer>
+                <IconImage src={twitterLogo.src} />
+              </ImageIconContainer>
+            }
+          />
+          <StyledInfoCellWithScroll
+            title="Linkedin"
+            content={
+              <TextLink href="https://www.linkedin.com/in/george-gillams-37537077" hrefExternal>
+                See my profile on Linkedin
+              </TextLink>
+            }
+            aux={
+              <ImageIconContainer>
+                <IconImage src={linkedInLogo.src} />
+              </ImageIconContainer>
+            }
+          />
+          <StyledInfoCellWithScroll
+            cellStyle={INFO_CELL_STYLES.dark}
+            title="GitHub"
+            content={
+              <TextLink href="https://github.com/georgegillams" hrefExternal>
+                See my profile on GitHub
+              </TextLink>
+            }
+            aux={
+              <ImageIconContainer>
+                <IconImage invertInDarkMode src={githubLogo.src} />
+              </ImageIconContainer>
+            }
+          />
+          <StyledInfoCellWithScroll
+            title="Email"
+            content={
+              <TextLink href="mailto:hello@georgegillams.co.uk" hrefExternal>
+                Email hello@georgegillams.co.uk
+              </TextLink>
+            }
+            aux={
+              <ImageIconContainer>
+                <IconImage invertInDarkMode src={emailLogo.src} />
+              </ImageIconContainer>
+            }
+          />
+          <StyledInfoCellWithScroll
+            cellStyle={INFO_CELL_STYLES.dark}
+            title="Flickr"
+            content={
+              <TextLink href="https://www.flickr.com/photos/georgegillams" hrefExternal>
+                See my profile on Flickr
+              </TextLink>
+            }
+            aux={
+              <ImageIconContainer>
+                <IconImage src={flickrLogo.src} />
+              </ImageIconContainer>
+            }
+          />
+          <StyledInfoCellWithScroll
+            title="Gurushots"
+            content={
+              <TextLink href="https://gurushots.com/georgegillams/photos" hrefExternal>
+                See my profile on Gurushots
+              </TextLink>
+            }
+            aux={
+              <ImageIconContainer>
+                <IconImage invertInDarkMode src={gurushotsLogo.src} />
+              </ImageIconContainer>
+            }
+          />
+          <StyledInfoCellWithScroll
+            cellStyle={INFO_CELL_STYLES.dark}
+            title="Unsplash"
+            content={
+              <TextLink href="https://unsplash.com/@georgegillams/" hrefExternal>
+                See my profile on Unsplash
+              </TextLink>
+            }
+            aux={
+              <ImageIconContainer>
+                <IconImage invertInDarkMode src={unsplashLogo.src} />
+              </ImageIconContainer>
+            }
+          />
+        </InfoCellContainer>
       </PageTitle>
     </PageContainer>
   );
