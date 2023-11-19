1da290425a989d8ba4621a222ad81ebb791db130
George Gillams <code@georgegillams.co.uk>
2023-11-19 19:56:49 +0000

    feat: Add babel-plugin-styled-components (#1054)
    * feat: Add babel-plugin-styled-components
    
    * fix: Update snapshots

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    'babel-plugin-add-module-exports',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          helpers: './shared/helpers',
+          'server-utils': './server/utils',
+          'client-utils': './src/utils',
+        },
+      },
+    ],
+    [
+      'styled-components',
+      {
+        ssr: true,
+        displayName: !isProduction,
+        fileName: !isProduction,
+        preprocess: false,
+      },
+    ],
+  ],
+};
