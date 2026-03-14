643cd4289cb0781db1fbb2959cf9ffa67c7f7ccc
George Gillams <code@georgegillams.co.uk>
2026-03-14 09:27:48 +0000

    fix: Fix yarn install on CI

--- src/quattrocento.styles.js
+++ src/quattrocento.styles.js
@@ -22,8 +22,9 @@ export const quattrocento = css`
       local('Quattrocento Sans'),
       local('QuattrocentoSans'),
       url('/static/fonts/quattrocento-sans-latin-400-normal.woff2') format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC,
-      U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    unicode-range:
+      U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122,
+      U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 
   /* latin-ext */
@@ -47,7 +48,8 @@ export const quattrocento = css`
       local('Quattrocento Sans Bold'),
       local('QuattrocentoSans-Bold'),
       url('/static/fonts/quattrocento-sans-latin-700-normal.woff2') format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC,
-      U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    unicode-range:
+      U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122,
+      U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 `;
