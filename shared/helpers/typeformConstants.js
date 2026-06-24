56ecdea8f0eb294209fd922f333fb0c7d74165f2
George Gillams <code@georgegillams.co.uk>
2026-06-24 08:37:27 +0100

    fix: Fix chorder feedback link

--- shared/helpers/typeformConstants.js
+++ shared/helpers/typeformConstants.js
@@ -1,4 +1,4 @@
 export const CONTACT_FORM = 'https://georgegillams.typeform.com/contact';
 export const LOST_FOUND_FORM = 'https://georgegillams.typeform.com/found';
 export const CONTRASTER_FEEDBACK_FORM = 'https://georgegillams.typeform.com/app-feedback?app_name=Contraster';
-export const CHORDER_FEEDBACK_FORM = 'https://georgegillams.typeform.com/app-feedback?app_name=Contraster';
+export const CHORDER_FEEDBACK_FORM = 'https://georgegillams.typeform.com/app-feedback?app_name=Chorder';
